1afe6239922678c1f72238ad6fc7e73bed7b973a
Matthew Mundell <matthew.mundell@greenbone.net>
2009-01-15 13:46:06 +0000

    Add file header comments.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-manager@2208 423fd1db-d629-0410-8442-d21db03e70f4

--- src/tests/strip_space_0.c
+++ src/tests/strip_space_0.c
@@ -1,4 +1,27 @@
-/* Test strip_space. */
+/* Test 0 of strip_space.
+ * $Id$
+ * Description: Test strip_space on "abcd".
+ *
+ * Authors:
+ * Matthew Mundell <matt@mundell.ukfsn.org>
+ *
+ * Copyright:
+ * Copyright (C) 2008 Intevation GmbH
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * or, at your option, any later version as published by the Free
+ * Software Foundation
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
