460b2a10fde95bbe1baf975237a39e4a1c234010
Matthew Mundell <matthew.mundell@greenbone.net>
2009-01-16 19:37:14 +0000

    Add simple test of XML OMP START_TASK.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-manager@2243 423fd1db-d629-0410-8442-d21db03e70f4

---
+++ src/tests/omp_start_task_0.c
@@ -0,0 +1,151 @@
+/* Test 0 of OMP MODIFY_TASK.
+ * $Id$
+ * Description: Test the OMP MODIFY_TASK command.
+ *
+ * Authors:
+ * Matthew Mundell <matt@mundell.ukfsn.org>
+ *
+ * Copyright:
+ * Copyright (C) 2009 Intevation GmbH
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * or, at your option, any later version as published by the Free
+ * Software Foundation
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#define TRACE 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "common.h"
+#include "../tracef.h"
+
+char* new_task_request = "<new_task><task_file># This file was automagically created by OpenVAS-Client\n\
+trusted_ca = cacert.pem\n\
+targets = impi\n\
+protocol_version = 0\n\
+cache_plugin_information = no\n\
+reports_use_plugin_cache = no\n\
+show_nvt_name_and_oid = yes\n\
+use_client_cert = no\n\
+nessusd_port = 7772\n\
+paranoia_level = 1\n\
+\n\
+begin(SCANNER_SET)\n\
+end(SCANNER_SET)\n\
+\n\
+begin(SERVER_PREFS)\n\
+ max_hosts = 20\n\
+ max_checks = 4\n\
+ cgi_path = /cgi-bin:/scripts\n\
+ port_range = default\n\
+ auto_enable_dependencies = yes\n\
+ silent_dependencies = yes\n\
+ host_expansion = ip\n\
+ ping_hosts = no\n\
+ reverse_lookup = no\n\
+ optimize_test = yes\n\
+ safe_checks = yes\n\
+ use_mac_addr = no\n\
+ unscanned_closed = no\n\
+ save_knowledge_base = yes\n\
+ only_test_hosts_whose_kb_we_dont_have = no\n\
+ only_test_hosts_whose_kb_we_have = no\n\
+ kb_restore = no\n\
+ kb_dont_replay_scanners = no\n\
+ kb_dont_replay_info_gathering = no\n\
+ kb_dont_replay_attacks = no\n\
+ kb_dont_replay_denials = no\n\
+ kb_max_age = 864000\n\
+ log_whole_attack = no\n\
+ language = english\n\
+ checks_read_timeout = 5\n\
+ non_simult_ports = 139, 445\n\
+ plugins_timeout = 320\n\
+ slice_network_addresses = no\n\
+ nasl_no_signature_check = yes\n\
+end(SERVER_PREFS)\n\
+\n\
+begin(CLIENTSIDE_USERRULES)\n\
+end(CLIENTSIDE_USERRULES)\n\
+\n\
+begin(PLUGINS_PREFS)\n\
+end(PLUGINS_PREFS)\n\
+\n\
+begin(PLUGIN_SET)\n\
+end(PLUGIN_SET)\n\
+\n\
+begin(SERVER_INFO)\n\
+ server_info_openvasd_version = 2.0.0\n\
+ server_info_libnasl_version = 2.0.0.beta3.SVN\n\
+ server_info_libnessus_version = 2.0.0.beta3.SVN\n\
+ server_info_thread_manager = fork\n\
+ server_info_os = Linux\n\
+ server_info_os_version = 2.6.18-6-486\n\
+end(SERVER_INFO)\n\
+</task_file><identifier>omp_start_task_0</identifier><comment>Test 0 of OMP START_TASK.</comment></new_task>\n";
+
+int
+main ()
+{
+  int socket;
+  gnutls_session_t session;
+
+  socket = connect_to_manager (&session);
+  if (socket == -1) return EXIT_FAILURE;
+
+  /* Create a task. */
+
+  if (send_to_manager (&session, new_task_request) == -1)
+    {
+      gnutls_bye (session, GNUTLS_SHUT_RDWR);
+      close (socket);
+      goto fail;
+    }
+
+  char* entity = read_entity (&session);
+  tracef ("new task entity: %s\n", entity);
+  // FIX get id, assume 0 for now
+
+  if (strcmp (entity, "new_task_response"))
+    goto fail;
+
+  /* Start the task. */
+
+  if (send_to_manager (&session,
+                       "<start_task><task_id>0</task_id></start_task>")
+      == -1)
+    goto fail;
+
+  /* Read the response. */
+
+  entity = read_entity (&session);
+  tracef ("entity: %s\n", entity);
+
+  /* Compare. */
+
+  if (strcmp (entity, "start_task_response") == 0)
+    {
+      gnutls_bye (session, GNUTLS_SHUT_RDWR);
+      close (socket);
+      return EXIT_SUCCESS;
+    }
+
+ fail:
+  gnutls_bye (session, GNUTLS_SHUT_RDWR);
+  close (socket);
+  return EXIT_FAILURE;
+}
