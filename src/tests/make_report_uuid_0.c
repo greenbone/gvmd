7d13981b44a448bad7b3b0dff03ad1bf927b8f73
Matthew Mundell <matthew.mundell@greenbone.net>
2009-05-22 09:07:04 +0000

    * src/tests/make_report_id_0.c: Rename make_report_uuid_0.c. 	* src/tests/report_path_task_name_0.c: Rename report_path_task_uuid_0.c. 	* src/tests/report_path_task_name_1.c: Rename report_path_task_uuid_1.c. 	* src/tests/report_path_task_name_2.c: Rename report_path_task_uuid_2.c.
    	* src/tests/make_report_uuid_0.c, src/tests/report_path_task_uuid_0.c,
    	src/tests/report_path_task_uuid_1.c,
    	src/tests/report_path_task_uuid_2.c: Update function names in header
    	comments.
    
    	* src/tests/CMakeLists.txt: Update file names.  Remove duplicate entries.
    
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-manager@3457 423fd1db-d629-0410-8442-d21db03e70f4

---
+++ src/tests/make_report_uuid_0.c
@@ -0,0 +1,42 @@
+/* Test 0 of make_report_uuid.
+ * $Id$
+ * Description: Test make_report_uuid.
+ *
+ * Authors:
+ * Matthew Mundell <matt@mundell.ukfsn.org>
+ *
+ * Copyright:
+ * Copyright (C) 2009 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * or, at your option, any later version as published by the Free
+ * Software Foundation
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../manage.h"
+
+int
+main ()
+{
+  char* id1 = make_report_uuid ();
+  char* id2 = make_report_uuid ();
+  int ret = strcmp (id1, id2);
+  free (id1);
+  free (id2);
+  if (ret) return EXIT_SUCCESS;
+  return EXIT_FAILURE;
+}
