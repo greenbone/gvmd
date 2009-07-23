99a2a35b5c07092f064d88aaf0874057ca54e8b7
Matthew Mundell <matthew.mundell@greenbone.net>
2009-07-23 21:47:44 +0000

    Match the db model better to the state of the manager.
    	* doc/db_postgres.sql (nvt_selectors, results, report_hosts)
    	(report_results): New tables.
    	(tasks): Add reference to nvt_selector.
    	(reports): Add date, start_time, end_time.
    
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-manager@4136 423fd1db-d629-0410-8442-d21db03e70f4

--- doc/db_postgres.sql
+++ doc/db_postgres.sql
@@ -3,17 +3,50 @@ CREATE TABLE users (
     name text UNIQUE NOT NULL,
 	password text);
 
+CREATE TABLE nvt_selectors (
+	rowid integer PRIMARY KEY,
+	id integer,
+    name text,
+	exclude boolean,
+	type integer,
+	details text);
+
 CREATE TABLE tasks (
 	id integer PRIMARY KEY,
 	uuid text UNIQUE NOT NULL,
 	name text,
 	owner integer REFERENCES users (id) ON DELETE RESTRICT,
+	nvt_selector integer REFERENCES nvt_selectors (id) ON DELETE RESTRICT,
 	rcfile text,
 	comment text);
+
+CREATE TABLE results (
+	id integer PRIMARY KEY,
+	task integer REFERENCES tasks (id) ON DELETE RESTRICT,
+	subnet text,
+	host text,
+	port text,
+	nvt text,  -- OID of NVT
+	type text,
+	description text);
 
 CREATE TABLE reports (
 	id integer PRIMARY KEY,
 	uuid text UNIQUE NOT NULL,
 	task integer REFERENCES tasks (id) ON DELETE RESTRICT,
+	date date,
+	start_time date,
+	end_time date,
 	nbefile text,
 	comment text);
+
+CREATE TABLE report_hosts (
+	id integer PRIMARY KEY,
+	report integer REFERENCES reports (id) ON DELETE RESTRICT,
+	host text,
+	start_time date,
+	end_time date);
+
+CREATE TABLE report_results (
+	report integer PRIMARY KEY REFERENCES reports (id) ON DELETE RESTRICT,
+	result integer PRIMARY KEY REFERENCES results (id) ON DELETE RESTRICT);
