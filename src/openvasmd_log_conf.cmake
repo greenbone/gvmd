3f37d7587c1456f122fd54edd9ed77a0fb69248d
Matthew Mundell <matthew.mundell@greenbone.net>
2009-07-28 11:08:39 +0000

    * src/openvasmd_log_conf.cmake: Remove quotes from "prepend" key.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-manager@4236 423fd1db-d629-0410-8442-d21db03e70f4

--- src/openvasmd_log_conf.cmake
+++ src/openvasmd_log_conf.cmake
@@ -1,60 +1,60 @@
 [libopenvas]
-prepend="%t %p"
+prepend=%t %p
 prepend_time_format=%Y%m%d%H%M%S
 file=${OPENVAS_LOG_DIR}/libopenvas.log
 
 [libnasl]
-prepend="%t %p"
+prepend=%t %p
 prepend_time_format=%Y%m%d%H%M%S
 file=${OPENVAS_LOG_DIR}/libnasl.log
 
 [openvasd]
-prepend="%t %p"
+prepend=%t %p
 prepend_time_format=%Y%m%d%H%M%S
 file=${OPENVAS_LOG_DIR}/openvasd.log
 
 [md   main]
-prepend="%t %p"
+prepend=%t %p
 prepend_time_format=%Y%m%d%H%M%S
 file=${OPENVAS_LOG_DIR}/openvasmd.log
 level=128
 
 [md   comm]
-prepend="%t %p"
+prepend=%t %p
 prepend_time_format=%Y%m%d%H%M%S
 file=${OPENVAS_LOG_DIR}/openvasmd.log
 level=128
 
 [md   file]
-prepend="%t %p"
+prepend=%t %p
 prepend_time_format=%Y%m%d%H%M%S
 file=${OPENVAS_LOG_DIR}/openvasmd.log
 level=128
 
 [md string]
-prepend="%t %p"
+prepend=%t %p
 prepend_time_format=%Y%m%d%H%M%S
 file=${OPENVAS_LOG_DIR}/openvasmd.log
 level=128
 
 [md manage]
-prepend="%t %p"
+prepend=%t %p
 prepend_time_format=%Y%m%d%H%M%S
 file=${OPENVAS_LOG_DIR}/openvasmd.log
 level=128
 
 [md    omp]
-prepend="%t %p"
+prepend=%t %p
 prepend_time_format=%Y%m%d%H%M%S
 file=${OPENVAS_LOG_DIR}/openvasmd.log
 level=128
 
 [md    otp]
-prepend="%t %p"
+prepend=%t %p
 prepend_time_format=%Y%m%d%H%M%S
 file=${OPENVAS_LOG_DIR}/openvasmd.log
 level=128
 
 [*]
-prepend="%p"
+prepend=%p
 file=${OPENVAS_LOG_DIR}/openvas.log
