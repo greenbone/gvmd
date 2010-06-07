a203bb3fd7d71f69c001431f89b97d32573b130f
Matthew Mundell <matthew.mundell@greenbone.net>
2010-06-07 20:35:58 +0000

    * src/omp.c (omp_xml_handle_end_element): In CLIENT_CREATE_TARGET add 	missing breaks.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-manager@7960 423fd1db-d629-0410-8442-d21db03e70f4

--- src/omp.c
+++ src/omp.c
@@ -9981,10 +9981,12 @@ omp_xml_handle_end_element (/*@unused@*/ GMarkupParseContext* context,
                 SEND_TO_CLIENT_OR_FAIL
                  (XML_ERROR_SYNTAX ("create_target",
                                     "Target exists already"));
+                break;
               case -1:
                 SEND_TO_CLIENT_OR_FAIL
                  (XML_ERROR_SYNTAX ("create_target",
                                     "Import from target_locator failed"));
+                break;
               default:
                 SEND_TO_CLIENT_OR_FAIL (XML_OK_CREATED ("create_target"));
                 break;
