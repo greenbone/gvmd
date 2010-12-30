b8d9b2549628dd62de6ec0bd4cc58a7681c3992d
Jan-Oliver Wagner <jan-oliver.wagner@greenbone.net>
2010-12-30 20:24:48 +0000

    * src/openvasmd.c (main): Added forgotten license note.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-manager@9838 423fd1db-d629-0410-8442-d21db03e70f4

--- src/openvasmd.c
+++ src/openvasmd.c
@@ -1076,6 +1076,7 @@ main (int argc, char** argv)
       printf ("OpenVAS Manager %s\n", OPENVASMD_VERSION);
       printf ("Manager DB revision %i\n", manage_db_supported_version ());
       printf ("Copyright (C) 2010 Greenbone Networks GmbH\n");
+      printf ("License GPLv2+: GNU GPL version 2 or later\n");
       printf
         ("This is free software: you are free to change and redistribute it.\n"
          "There is NO WARRANTY, to the extent permitted by law.\n\n");
