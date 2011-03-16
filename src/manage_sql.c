3915c8c766f8231d438be9aa29b45c606953450d
Matthew Mundell <matthew.mundell@greenbone.net>
2011-03-16 04:24:27 +0000

    * src/manage_sql.c (init_task_iterator): Correct test in last commit for 	the all case.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-manager@10553 423fd1db-d629-0410-8442-d21db03e70f4

--- src/manage_sql.c
+++ src/manage_sql.c
@@ -6408,7 +6408,7 @@ init_task_iterator (iterator_t* iterator,
                        "%s"
                        " ORDER BY %s %s;",
                        current_credentials.uuid,
-                       trash ? " AND hidden = 2" : " AND hidden < 0",
+                       trash ? " AND hidden = 2" : " AND hidden < 2",
                        sort_field ? sort_field : "ROWID",
                        ascending ? "ASC" : "DESC");
     }
@@ -6429,7 +6429,7 @@ init_task_iterator (iterator_t* iterator,
                        "SELECT ROWID, uuid, run_status FROM tasks"
                        "%s"
                        " ORDER BY %s %s;",
-                       trash ? " WHERE hidden = 2" : " WHERE hidden < 0",
+                       trash ? " WHERE hidden = 2" : " WHERE hidden < 2",
                        sort_field ? sort_field : "ROWID",
                        ascending ? "ASC" : "DESC");
     }
