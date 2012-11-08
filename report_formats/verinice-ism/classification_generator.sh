8f7a859ff6777cd17ff41d7e745c189cd8e418bb
Andre Heinecke <andre.heinecke@intevation.de>
2012-11-08 11:38:52 +0000

    * report_formats/verinice-ism/classification_generator.sh: New.     Generator to create XSL helper template for verinice system tags from     a csv file.     * report_formats/verinice-ism/classification.csv: New. CSV File     containting CPE's and tags for verinice ism
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-manager@14469 423fd1db-d629-0410-8442-d21db03e70f4

---
+++ report_formats/verinice-ism/classification_generator.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+#
+# OpenVAS Manager
+# $Id$
+# Description: Generate classification helpers for verinice
+#
+# Authors:
+# Andre Heinecke <andre.heinecke@greenbone.net>
+#
+# Copyright:
+# Copyright (C) 2011 Greenbone Networks GmbH
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2,
+# or, at your option, any later version as published by the Free
+# Software Foundation
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+cat << EOF
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+<xsl:template name="generate-tags">
+  <xsl:choose>
+    <xsl:when test="name='OS' or name='best_os_cpe'">
+      <xsl:choose>
+EOF
+
+for line in $(tail -n +2 "$1")
+do
+    CPE=$(echo $line | awk -F , '{print $1}')
+    TAG=$(echo $line | awk -F , '{print $2}')
+    echo "        <xsl:when test=\"contains(value, '${CPE}')\">"
+    echo "          <xsl:text>${TAG}</xsl:text>"
+    echo "        </xsl:when>"
+done
+
+cat << EOF
+      <xsl:otherwise>
+        <xsl:text>gsm_system_unkown</xsl:text>"
+      </xsl:otherwise>
+    </xsl:choose>
+  </xsl:choose>
+</xsl:template>
+EOF
