2d987359368edbf81f06f32ac87459135843df1d
Matthew Mundell <matthew.mundell@greenbone.net>
2012-12-13 11:37:18 +0000

    * src/manage_sql.c (init_get_iterator): Drop the owner clause when 	getting a single resource, because the find_resource_for_actions does 	the owner check.  This makes the iterator work again for single 	observed tasks.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-manager@14814 423fd1db-d629-0410-8442-d21db03e70f4

--- src/manage_sql.c
+++ src/manage_sql.c
@@ -3237,11 +3237,11 @@ init_get_iterator (iterator_t* iterator, const char *type,
 
   if (owned)
     {
-      if (current_credentials.uuid == NULL)
+      if (resource || (current_credentials.uuid == NULL))
         owned_and_used_by_clause
          = g_strdup_printf (" (%s)",
                             used_by_clause ? used_by_clause : "1");
-      else if (resource || get->trash)
+      else if (get->trash)
         owned_and_used_by_clause = g_strdup_printf (" ((owner IS NULL) OR (owner ="
                                                     "  (SELECT ROWID FROM users"
                                                     "   WHERE users.uuid = '%s'))"
