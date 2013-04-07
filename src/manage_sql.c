5aca57f9c10e3149adc481a66107791579a28513
Jan-Oliver Wagner <jan-oliver.wagner@greenbone.net>
2013-04-07 18:51:11 +0000

    * src/manage_sql.c (setup_full_config_prefs): Remove handling of "silent_dependencies".
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-manager@15942 423fd1db-d629-0410-8442-d21db03e70f4

--- src/manage_sql.c
+++ src/manage_sql.c
@@ -13907,9 +13907,6 @@ setup_full_config_prefs (config_t config, int safe_checks,
        " VALUES (%i, 'SERVER_PREFS', 'auto_enable_dependencies', 'yes');",
        config);
   sql ("INSERT into config_preferences (config, type, name, value)"
-       " VALUES (%i, 'SERVER_PREFS', 'silent_dependencies', 'yes');",
-       config);
-  sql ("INSERT into config_preferences (config, type, name, value)"
        " VALUES (%i, 'SERVER_PREFS', 'host_expansion', 'ip');",
        config);
   sql ("INSERT into config_preferences (config, type, name, value)"
