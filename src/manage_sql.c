071eace31e375b964bee6735fc0c657c59a520d0
Hani Benhabiles <hani.benhabiles@greenbone.net>
2013-05-08 11:45:40 +0000

    * src/manage_sql.c (print_report_xml): Remove unused duplicate code for closed cves.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-manager@16257 423fd1db-d629-0410-8442-d21db03e70f4

--- src/manage_sql.c
+++ src/manage_sql.c
@@ -27698,7 +27698,6 @@ print_report_xml (report_t report, report_t delta, task_t task, gchar* xml_file,
           if (present)
             {
               iterator_t details;
-              guint index;
               array_t *cves;
 
               cves = make_array ();
@@ -27737,36 +27736,6 @@ print_report_xml (report_t report, report_t delta, task_t task, gchar* xml_file,
                          report_host_details_iterator_source_desc (&details));
                 }
               cleanup_iterator (&details);
-
-              if (cves->len > 0)
-                {
-                  GString *detail_cves;
-
-                  /* Combine all buffered CVEs into one host detail. */
-
-                  g_ptr_array_sort (cves, compare_strings);
-
-                  detail_cves = g_string_new ((gchar*) g_ptr_array_index (cves,
-                                                                          0));
-                  for (index = 1; index < cves->len; index++)
-                    g_string_append_printf
-                     (detail_cves,
-                      ", %s",
-                      (gchar*) g_ptr_array_index (cves, index));
-                  PRINT (out,
-                         "<detail>"
-                         "<name>Closed CVEs</name>"
-                         "<value>%s</value>"
-                         "<source>"
-                         "<type>openvasmd</type>"
-                         "<name></name>"
-                         "<description></description>"
-                         "</source>"
-                         "</detail>",
-                         detail_cves->str);
-                  g_string_free (detail_cves, TRUE);
-                }
-              array_free (cves);
 
               PRINT (out,
                      "</host>");
