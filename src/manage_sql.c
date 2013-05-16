72383255e0af6533f780945389c260ada309669a
Hani Benhabiles <hani.benhabiles@greenbone.net>
2013-05-16 10:14:13 +0000

    * src/manage_sql.c (report_os_count): New function. (print_report_xml): Send report's total number of detected Operating Systems.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-manager@16354 423fd1db-d629-0410-8442-d21db03e70f4

--- src/manage_sql.c
+++ src/manage_sql.c
@@ -25748,6 +25748,22 @@ report_vuln_count (report_t report)
 }
 
 /**
+ * @brief Count a report's total number of detected Operating Systems.
+ *
+ * @return OS count.
+ */
+static int
+report_os_count (report_t report)
+{
+  return sql_int (0, 0,
+                  "SELECT count (DISTINCT value) FROM report_host_details"
+                  " WHERE report_host IN"
+                  "  (SELECT ROWID from report_hosts WHERE report = %llu)"
+                  "  AND name = 'best_os_txt';",
+                  report);
+}
+
+/**
  * @brief Print the XML for a report to a file.
  *
  * @param[in]  report      The report.
@@ -26123,6 +26139,10 @@ print_report_xml (report_t report, report_t delta, task_t task, gchar* xml_file,
       PRINT (out,
              "<vulns total=\"%i\"/>",
              report_vuln_count (report));
+
+      PRINT (out,
+             "<os total=\"%i\"/>",
+             report_os_count (report));
     }
 
   if (task && tsk_uuid)
