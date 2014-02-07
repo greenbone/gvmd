29f7101e5ae0fbb833bacef3b6cad80e94e02e3e
Matthew Mundell <matthew.mundell@greenbone.net>
2014-02-07 08:20:10 +0000

    Backport r18655.
    	* src/manage.c (stop_task): Only check if scanner is up when scanner is
    	required.
    
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/branches/openvas-manager-4.0@18718 423fd1db-d629-0410-8442-d21db03e70f4

--- src/manage.c
+++ src/manage.c
@@ -2876,8 +2876,6 @@ stop_task (task_t task)
 {
   task_status_t run_status;
   tracef ("   request task stop %u\n", task_id (task));
-  if (scanner_up == 0)
-    return -5;
   run_status = task_run_status (task);
   if (run_status == TASK_STATUS_PAUSE_REQUESTED
       || run_status == TASK_STATUS_PAUSE_WAITING
@@ -2887,9 +2885,13 @@ stop_task (task_t task)
       || run_status == TASK_STATUS_REQUESTED
       || run_status == TASK_STATUS_RUNNING)
     {
-      if (current_scanner_task == task
-          && send_to_server ("CLIENT <|> STOP_WHOLE_TEST <|> CLIENT\n"))
-        return -1;
+      if (current_scanner_task == task)
+        {
+          if (scanner_up == 0)
+            return -5;
+          if (send_to_server ("CLIENT <|> STOP_WHOLE_TEST <|> CLIENT\n"))
+            return -1;
+        }
       set_task_run_status (task, TASK_STATUS_STOP_REQUESTED);
       return 1;
     }
