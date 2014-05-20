e4e5780b6dbeb225ca565e2a723c1687678c3ddf
Matthew Mundell <matthew.mundell@greenbone.net>
2014-05-20 18:55:59 +0000

    Move SQLite specific code in the SQL library to a separate file.
    	* src/sql.c (DB_CHUNK_SIZE, task_db, sql_open, sql_close, sql_changes)
    	(sql_last_insert_rowid, sql_prepare_internal, sql_exec_internal)
    	(iterator_null, iterator_column_name, iterator_column_count)
    	(sql_bind_blob, sql_bind_int64, sql_bind_double, sql_bind_text)
    	(sql_finalize, sql_reset, sql_column_double, sql_column_text)
    	(sql_column_int, sql_column_int64): Remove.  Now in sql_sqlite3.c.  Add
    	headers for internal functions.
    
    	* src/sql_sqlite3.c: New file.
    
    	* src/CMakeLists.txt: Add sql_sqlite3.c.
    
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-manager@19493 423fd1db-d629-0410-8442-d21db03e70f4

--- src/sql.c
+++ src/sql.c
@@ -29,83 +29,17 @@
 #include <assert.h>
 #include <string.h>
 
-/**
- * @brief Chunk size for SQLite memory allocation.
- */
-#define DB_CHUNK_SIZE 1 * 1024 * 1024
-
 
-/* Variables. */
+/* Headers of internal symbols defined in backend files. */
 
-/**
- * @brief Handle on the database.
- */
-sqlite3* task_db;
+int
+sql_prepare_internal (int, int, const char*, va_list, sql_stmt_t **);
+
+int
+sql_exec_internal (int, sql_stmt_t *);
 
 
 /* Helpers. */
-
-/**
- * @brief Open the database.
- *
- * @param[in]  database  Database, or NULL for default.
- *
- * @return 0 success, -1 error.
- */
-int
-sql_open (const char *database)
-{
-  int chunk_size = DB_CHUNK_SIZE;
-
-  if (sqlite3_open (database ? database : OPENVAS_STATE_DIR "/mgr/tasks.db",
-                    &task_db))
-    {
-      g_warning ("%s: sqlite3_open failed: %s\n",
-                 __FUNCTION__,
-                 sqlite3_errmsg (task_db));
-      return -1;
-    }
-
-  sqlite3_file_control (task_db, NULL, SQLITE_FCNTL_CHUNK_SIZE, &chunk_size);
-  return 0;
-}
-
-/**
- * @brief Close the database.
- */
-void
-sql_close ()
-{
-  if (sqlite3_close (task_db) == SQLITE_BUSY)
-    /* Richard Hipp on how to find the open statements:
-     *
-     * There is no published way to do this.  If you run in a debugger,
-     * you can look at the linked list of "struct Vdbe" objects that
-     * sqlite3.pVdbe points to.  This is the list of open statements
-     * in the current implementation (and subject to change without
-     * notice). */
-    g_warning ("%s: attempt to close db with open statement(s)\n",
-               __FUNCTION__);
-  task_db = NULL;
-}
-
-/**
- * @brief Get the number of rows changed or inserted in last statement.
- */
-int
-sql_changes ()
-{
-  return sqlite3_changes (task_db);
-}
-
-/**
- * @brief Get the ID of the last inserted row.
- */
-resource_t
-sql_last_insert_rowid ()
-{
-  return sqlite3_last_insert_rowid (task_db);
-}
 
 /**
  * @brief Quotes a string of a known length to be passed to sql statements.
@@ -186,115 +120,6 @@ sql_insert (const char *string)
       return insert;
     }
   return g_strdup ("NULL");
-}
-
-/**
- * @brief Prepare a statement.
- *
- * @param[in]  retry  Whether to keep retrying while database is busy or locked.
- * @param[in]  log    Whether to keep retrying while database is busy or locked.
- * @param[in]  sql    Format string for SQL statement.
- * @param[in]  args   Arguments for format string.
- * @param[out] stmt   Statement return.
- *
- * @return 0 success, 1 gave up, -1 error.
- */
-int
-sql_prepare_internal (int retry, int log, const char* sql, va_list args,
-                      sql_stmt_t **stmt)
-{
-  const char* tail;
-  int ret, retries;
-  gchar* formatted;
-
-  formatted = g_strdup_vprintf (sql, args);
-
-  if (log)
-    tracef ("   sql: %s\n", formatted);
-
-  retries = 10;
-  *stmt = NULL;
-  while (1)
-    {
-      ret = sqlite3_prepare (task_db, (char*) formatted, -1, stmt, &tail);
-      if (ret == SQLITE_BUSY || ret == SQLITE_LOCKED)
-        {
-          if (retry)
-            continue;
-          if (retries--)
-            continue;
-          g_free (formatted);
-          return 1;
-        }
-      g_free (formatted);
-      if (ret == SQLITE_OK)
-        {
-          if (*stmt == NULL)
-            {
-              g_warning ("%s: sqlite3_prepare failed with NULL stmt: %s\n",
-                         __FUNCTION__,
-                         sqlite3_errmsg (task_db));
-              return -1;
-            }
-          break;
-        }
-      g_warning ("%s: sqlite3_prepare failed: %s\n",
-                 __FUNCTION__,
-                 sqlite3_errmsg (task_db));
-      return -1;
-    }
-
-  return 0;
-}
-
-/**
- * @brief Execute a prepared statement.
- *
- * @param[in]  retry  Whether to keep retrying while database is busy or locked.
- * @param[in]  stmt   Statement.
- *
- * @return 0 complete, 1 row available in results, -1 error, -2 gave up.
- */
-static int
-sql_exec_internal (int retry, sql_stmt_t *stmt)
-{
-  int retries;
-
-  retries = 10;
-  while (1)
-    {
-      int ret;
-      ret = sqlite3_step (stmt);
-      if (ret == SQLITE_BUSY)
-        {
-          if (retry)
-            continue;
-          if (retries--)
-            continue;
-          return -2;
-        }
-      if (ret == SQLITE_DONE)
-        return 0;
-      if (ret == SQLITE_ERROR || ret == SQLITE_MISUSE)
-        {
-          if (ret == SQLITE_ERROR)
-            {
-              ret = sqlite3_reset (stmt);
-              if (ret == SQLITE_BUSY || ret == SQLITE_LOCKED)
-                {
-                  if (retry)
-                    continue;
-                  return -2;
-                }
-            }
-          g_warning ("%s: sqlite3_step failed: %s\n",
-                     __FUNCTION__,
-                     sqlite3_errmsg (task_db));
-          return -1;
-        }
-      assert (ret == SQLITE_ROW);
-      return 1;
-    }
 }
 
 /**
@@ -770,21 +595,6 @@ iterator_int64 (iterator_t* iterator, int col)
 }
 
 /**
- * @brief Get whether a column is NULL.
- *
- * @param[in]  iterator  Iterator.
- * @param[in]  col       Column offset.
- *
- * @return 1 if NULL, else 0.
- */
-int
-iterator_null (iterator_t* iterator, int col)
-{
-  if (iterator->done) abort ();
-  return sqlite3_column_type (iterator->stmt, col) == SQLITE_NULL;
-}
-
-/**
  * @brief Get a string column from an iterator.
  *
  * @param[in]  iterator  Iterator.
@@ -797,35 +607,6 @@ iterator_string (iterator_t* iterator, int col)
 {
   if (iterator->done) abort ();
   return sql_column_text (iterator->stmt, col);
-}
-
-/**
- * @brief Get a column name from an iterator.
- *
- * @param[in]  iterator  Iterator.
- * @param[in]  col       Column offset.
- *
- * @return Name of given column.
- */
-const char*
-iterator_column_name (iterator_t* iterator, int col)
-{
-  if (iterator->done) abort ();
-  return (const char*) sqlite3_column_name (iterator->stmt, col);
-}
-
-/**
- * @brief Get number of columns from an iterator.
- *
- * @param[in]  iterator  Iterator.
- *
- * @return Number of columns.
- */
-int
-iterator_column_count (iterator_t* iterator)
-{
-  if (iterator->done) abort ();
-  return sqlite3_column_count (iterator->stmt);
 }
 
 /**
@@ -907,122 +688,6 @@ sql_prepare (const char* sql, ...)
 }
 
 /**
- * @brief Bind a blob to a statement.
- *
- * @param[in]  stmt        Statement.
- * @param[in]  position    Position in statement.
- * @param[in]  value       Blob.
- * @param[in]  value_size  Blob size.
- *
- * @return 0 success, -1 error.
- */
-int
-sql_bind_blob (sql_stmt_t *stmt, int position, const void *value,
-               int value_size)
-{
-  while (1)
-    {
-      int ret;
-      ret = sqlite3_bind_blob (stmt,
-                               position,
-                               value,
-                               value_size,
-                               SQLITE_TRANSIENT);
-      if (ret == SQLITE_BUSY) continue;
-      if (ret == SQLITE_OK) break;
-      g_warning ("%s: sqlite3_bind_blob failed: %s\n",
-                 __FUNCTION__,
-                 sqlite3_errmsg (task_db));
-      return -1;
-    }
-  return 0;
-}
-
-/**
- * @brief Bind an int64 value to a statement.
- *
- * @param[in]  stmt        Statement.
- * @param[in]  position    Position in statement.
- * @param[in]  value       Value.
- *
- * @return 0 success, -1 error.
- */
-int
-sql_bind_int64 (sql_stmt_t *stmt, int position, long long int value)
-{
-  while (1)
-    {
-      int ret;
-      ret = sqlite3_bind_int64 (stmt, position, value);
-      if (ret == SQLITE_BUSY) continue;
-      if (ret == SQLITE_OK) break;
-      g_warning ("%s: sqlite3_bind_int64 failed: %s\n",
-                 __FUNCTION__,
-                 sqlite3_errmsg (task_db));
-      return -1;
-    }
-  return 0;
-}
-
-/**
- * @brief Bind a double value to a statement.
- *
- * @param[in]  stmt        Statement.
- * @param[in]  position    Position in statement.
- * @param[in]  value       Value.
- *
- * @return 0 success, -1 error.
- */
-int
-sql_bind_double (sql_stmt_t *stmt, int position, double value)
-{
-  while (1)
-    {
-      int ret;
-      ret = sqlite3_bind_double (stmt, position, value);
-      if (ret == SQLITE_BUSY) continue;
-      if (ret == SQLITE_OK) break;
-      g_warning ("%s: sqlite3_bind_double failed: %s\n",
-                 __FUNCTION__,
-                 sqlite3_errmsg (task_db));
-      return -1;
-    }
-  return 0;
-}
-
-/**
- * @brief Bind a text value to a statement.
- *
- * @param[in]  stmt        Statement.
- * @param[in]  position    Position in statement.
- * @param[in]  value       Value.
- * @param[in]  value_size  Value size, or -1 to use strlen of value.
- *
- * @return 0 success, -1 error.
- */
-int
-sql_bind_text (sql_stmt_t *stmt, int position, const gchar *value,
-               gsize value_size)
-{
-  while (1)
-    {
-      int ret;
-      ret = sqlite3_bind_text (stmt,
-                               position,
-                               value,
-                               value_size,
-                               SQLITE_TRANSIENT);
-      if (ret == SQLITE_BUSY) continue;
-      if (ret == SQLITE_OK) break;
-      g_warning ("%s: sqlite3_bind_text failed: %s\n",
-                 __FUNCTION__,
-                 sqlite3_errmsg (task_db));
-      return -1;
-    }
-  return 0;
-}
-
-/**
  * @brief Execute a prepared statement.
  *
  * @param[in]  stmt  Statement.
@@ -1033,99 +698,4 @@ int
 sql_exec (sql_stmt_t *stmt)
 {
   return sql_exec_internal (1, stmt);
-}
-
-/**
- * @brief Free a prepared statement.
- *
- * @param[in]  stmt  Statement.
- */
-void
-sql_finalize (sql_stmt_t *stmt)
-{
-  sqlite3_finalize (stmt);
-}
-
-/**
- * @brief Reset a prepared statement.
- *
- * @param[in]  stmt  Statement.
- *
- * @return 0 success, -1 error.
- */
-int
-sql_reset (sql_stmt_t *stmt)
-{
-  sqlite3_clear_bindings (stmt);
-  while (1)
-    {
-      int ret;
-      ret = sqlite3_reset (stmt);
-      if (ret == SQLITE_BUSY) continue;
-      if (ret == SQLITE_DONE || ret == SQLITE_OK) break;
-      if (ret == SQLITE_ERROR || ret == SQLITE_MISUSE)
-        {
-          g_warning ("%s: sqlite3_reset failed: %s\n",
-                     __FUNCTION__,
-                     sqlite3_errmsg (task_db));
-          return -1;
-        }
-    }
-  return 0;
-}
-
-/**
- * @brief Return a column as a double from a prepared statement.
- *
- * @param[in]  stmt      Statement.
- * @param[in]  position  Column position.
- *
- * @return 0 success, -1 error.
- */
-double
-sql_column_double (sql_stmt_t *stmt, int position)
-{
-  return sqlite3_column_double (stmt, position);
-}
-
-/**
- * @brief Return a column as text from a prepared statement.
- *
- * @param[in]  stmt      Statement.
- * @param[in]  position  Column position.
- *
- * @return 0 success, -1 error.
- */
-const char *
-sql_column_text (sql_stmt_t *stmt, int position)
-{
-  return (const char*) sqlite3_column_text (stmt, position);
-}
-
-/**
- * @brief Return a column as an integer from a prepared statement.
- *
- * @param[in]  stmt      Statement.
- * @param[in]  position  Column position.
- *
- * @return 0 success, -1 error.
- */
-int
-sql_column_int (sql_stmt_t *stmt, int position)
-{
-  return sqlite3_column_int (stmt, position);
-}
-
-/**
- * @brief Return a column as an int64 from a prepared statement.
- *
- * @param[in]  stmt      Statement.
- * @param[in]  position  Column position.
- *
- * @return 0 success, -1 error.
- */
-long long int
-sql_column_int64 (sql_stmt_t *stmt, int position)
-{
-  return sqlite3_column_int64 (stmt, position);
 }
