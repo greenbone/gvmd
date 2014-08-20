6378f15349b1db7a777b8b7a74c894bee6146e91
Matthew Mundell <matthew.mundell@greenbone.net>
2014-08-20 13:48:24 +0000

    Backport r20145.
    	* src/manage.c (slave_setup): When the slave status is Done, update the
    	master progress just before setting the master status, instead of before
    	updating the results, otherwise the progress is -1 while the status is
    	still Running.
    
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/branches/openvas-manager-5.0@20146 423fd1db-d629-0410-8442-d21db03e70f4

--- src/manage.c
+++ src/manage.c
@@ -2391,7 +2391,8 @@ slave_setup (slave_t slave, gnutls_session_t *session, int *socket,
               || (run_status == TASK_STATUS_PAUSED))
             set_task_run_status (task, TASK_STATUS_RUNNING);
 
-          if (update_slave_progress (get_tasks))
+          if ((strcmp (status, "Running") == 0)
+              && update_slave_progress (get_tasks))
             {
               free_entity (get_tasks);
               goto fail_stop_task;
@@ -2547,6 +2548,12 @@ slave_setup (slave_t slave, gnutls_session_t *session, int *socket,
             }
 
           free_entity (get_report);
+          if (update_slave_progress (get_tasks))
+            {
+              free_entity (get_tasks);
+              goto fail_stop_task;
+            }
+          free_entity (get_tasks);
           set_task_run_status (task, TASK_STATUS_DONE);
           break;
         }
