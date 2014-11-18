e7568e4e8596a55bc846a689b69b7b3908c7ee31
Timo Pollmeier <timo.pollmeier@greenbone.net>
2014-11-18 09:47:05 +0000

    Make the OMP version a macro defined centrally in the CMake files 	like the database and daemon versions.
    	* CMakeLists.txt: New OMP_VERSION variable. Configure the file
    	src/schema_formats/XML/OMP.xml.in to insert value of OMP_VERSION here.
    	Install generate src/schema_formats/XML/OMP.xml from binary directory.
    
    	* doc/CMakeLists.txt: Use OMP.xml generated in binary directory.
    
    	* src/CMakeLists.txt: New add_definitions to add OMP_VERSION macro.
    
    	* src/omp.c (omp_xml_handle_end_element): In CLIENT_GET_VERSION and
    	CLIENT_GET_VERSION_AUTHENTIC cases use OMP_VERSION as version number.
    
    	* src/schema_formats/XML/OMP.xml: Remove. Renamed to OMP.xml.in.
    
    	* src/schema_formats/XML/OMP.xml.in: New. Renamed OMP.xml. Use the
    	@OMP_VERSION@ placeholder.
    
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-manager@20881 423fd1db-d629-0410-8442-d21db03e70f4

--- src/omp.c
+++ src/omp.c
@@ -17562,7 +17562,7 @@ omp_xml_handle_end_element (/*@unused@*/ GMarkupParseContext* context,
         SEND_TO_CLIENT_OR_FAIL ("<get_version_response"
                                 " status=\"" STATUS_OK "\""
                                 " status_text=\"" STATUS_OK_TEXT "\">"
-                                "<version>6.0</version>"
+                                "<version>" OMP_VERSION "</version>"
                                 "</get_version_response>");
         if (client_state == CLIENT_GET_VERSION_AUTHENTIC)
           set_client_state (CLIENT_AUTHENTIC);
