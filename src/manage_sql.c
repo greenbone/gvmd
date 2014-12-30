399aea978231ac1a5319aca77df3a3e9ea73f703
Hani Benhabiles <hani.benhabiles@greenbone.net>
2014-12-30 11:37:42 +0000

    * src/manage_sql.c (init_report_errors_iterator): Use UNION ALL for UNION as it doesn't remove dpulicates and provides better performance.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-manager@21253 423fd1db-d629-0410-8442-d21db03e70f4

--- src/manage_sql.c
+++ src/manage_sql.c
@@ -17149,7 +17149,7 @@ init_report_errors_iterator (iterator_t* iterator, report_t report)
                    " WHERE results.type = 'Error Message'"
                    "  AND results.nvt = nvts.oid"
                    "  AND results.report = %llu"
-                   " UNION SELECT results.host, results.port, results.nvt,"
+                   " UNION ALL SELECT results.host, results.port, results.nvt,"
                    "  results.description, '', '',"
                    "  results.nvt_version, results.severity"
                    "  FROM results"
