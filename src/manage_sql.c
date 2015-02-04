d751cc425b74d45c7dec5bb0d3906efc4d51feae
Matthew Mundell <matthew.mundell@greenbone.net>
2015-02-04 13:26:30 +0000

    * src/manage_sql.c (trim_partial_report): Remove report_hosts end_time 	'' checks, because end_time is now an integer.  For Postgres.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-manager@21558 423fd1db-d629-0410-8442-d21db03e70f4

--- src/manage_sql.c
+++ src/manage_sql.c
@@ -19554,7 +19554,7 @@ trim_partial_report (report_t report)
        "  WHERE results.report = %llu"
        "  AND report_hosts.report = %llu"
        "  AND results.host = report_hosts.host"
-       "  AND (report_hosts.end_time is NULL OR report_hosts.end_time = ''));",
+       "  AND report_hosts.end_time is NULL);",
        report,
        report);
 
@@ -19563,12 +19563,12 @@ trim_partial_report (report_t report)
   sql ("DELETE FROM report_host_details WHERE report_host IN"
        " (SELECT report_hosts.id FROM report_hosts"
        "  WHERE report_hosts.report = %llu"
-       "  AND (report_hosts.end_time is NULL OR report_hosts.end_time = ''));",
+       "  AND report_hosts.end_time is NULL);",
        report);
 
   sql ("DELETE FROM report_hosts"
        " WHERE report = %llu"
-       " AND (end_time is NULL OR end_time = '');",
+       " AND end_time is NULL;",
        report);
 
   /* Clear and rebuild counts cache */
