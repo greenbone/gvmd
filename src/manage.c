0aac053220f822682a15d5ae991f8e4d64ce9011
Matthew Mundell <matthew.mundell@greenbone.net>
2015-02-09 12:49:57 +0000

    * src/manage.c (slave_setup): Also specify the SSH port when creating the 	target.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-manager@21583 423fd1db-d629-0410-8442-d21db03e70f4

--- src/manage.c
+++ src/manage.c
@@ -1957,9 +1957,10 @@ slave_setup (slave_t slave, gnutls_session_t *session, int *socket,
       init_user_target_iterator (&targets, target);
       if (next (&targets))
         {
-          const char *hosts;
+          const char *hosts, *port;
           gchar *hosts_copy, *port_range;
           omp_create_target_opts_t opts;
+          int ssh_port;
 
           hosts = target_iterator_hosts (&targets);
           if (hosts == NULL)
@@ -1968,6 +1969,12 @@ slave_setup (slave_t slave, gnutls_session_t *session, int *socket,
               goto fail_credential;
             }
 
+          port = target_iterator_ssh_port (&targets);
+          if (port == NULL)
+            ssh_port = 0;
+          else
+            ssh_port = atoi (port);
+
           hosts_copy = g_strdup (hosts);
           port_range = target_port_range (get_iterator_resource (&targets));
           cleanup_iterator (&targets);
@@ -1975,6 +1982,7 @@ slave_setup (slave_t slave, gnutls_session_t *session, int *socket,
           opts = omp_create_target_opts_defaults;
           opts.hosts = hosts_copy;
           opts.ssh_credential_id = slave_ssh_credential_uuid;
+          opts.ssh_credential_port = ssh_port;
           opts.smb_credential_id = slave_smb_credential_uuid;
           opts.port_range = port_range;
           opts.name = name;
