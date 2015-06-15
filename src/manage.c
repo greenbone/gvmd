8c06b2c264fab3c6a1c8d91b9c64e902f7cf083c
Matthew Mundell <matthew.mundell@greenbone.net>
2015-06-15 14:09:04 +0000

    Backport r22637.
    	* src/manage.c (manage_schedule): Add sigmask_current arg.  Restore this
    	sigmask in forked child, so that it has sensible signal handling.
    
    	* src/manage.h: Update header accordingly.
    
    	* src/openvasmd.c (serve_and_schedule): Pass sigmask_current to
    	manage_schedule.
    
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/branches/openvas-manager-6.0@22639 423fd1db-d629-0410-8442-d21db03e70f4

--- src/manage.c
+++ src/manage.c
@@ -4651,6 +4651,7 @@ reschedule_task (const gchar *task_id)
  *                              to the Manager.  Must return PID in parent, 0
  *                              in child, or -1 on error.
  * @param[in]  run_tasks        Whether to run scheduled tasks.
+ * @param[in]  sigmask_current  Sigmask to restore in child.
  *
  * @return 0 success, 1 failed to get lock, -1 error.
  */
@@ -4659,7 +4660,8 @@ manage_schedule (int (*fork_connection) (int *,
                                          gnutls_session_t *,
                                          gnutls_certificate_credentials_t *,
                                          gchar*),
-                 gboolean run_tasks)
+                 gboolean run_tasks,
+                 sigset_t *sigmask_current)
 {
   iterator_t schedules;
   GSList *starts = NULL, *stops = NULL;
@@ -4800,6 +4802,10 @@ manage_schedule (int (*fork_connection) (int *,
           case 0:
             /* Child.  Carry on to start the task, reopen the database (required
              * after fork). */
+
+            /* Restore the sigmask that was blanked for pselect. */
+            pthread_sigmask (SIG_SETMASK, sigmask_current, NULL);
+
             reinit_manage_process ();
             while (starts)
               {
