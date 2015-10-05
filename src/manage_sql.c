e7c8983f010c3f8d06f3946b3b46402e57767ee5
Matthew Mundell <matthew.mundell@greenbone.net>
2015-10-05 12:08:03 +0000

    Backport r22979.
    	* src/manage_sql.c (task_severity): Remove extra FROM clause.
    
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/branches/openvas-manager-6.0@23502 423fd1db-d629-0410-8442-d21db03e70f4

--- src/manage_sql.c
+++ src/manage_sql.c
@@ -14445,8 +14445,6 @@ task_severity (task_t task, int overrides, int min_qod, int offset)
                             "                 WHERE nvts.oid"
                             "                         = results.nvt),"
                             "                results.severity)"
-                            "       FROM nvts"
-                            "       WHERE nvts.oid = results.nvt)"
                             " ELSE results.severity END");
   else
     severity_sql = g_strdup ("results.severity");
