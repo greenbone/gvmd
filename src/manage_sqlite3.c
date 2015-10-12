511b77800f31261da71a5b99b7067348f5c1d374
Matthew Mundell <matthew.mundell@greenbone.net>
2015-10-12 10:23:40 +0000

    * src/manage_sqlite3.c (create_tables): Lock the db completely around 	the drop-create view steps, so that other processes do not try to access 	the views when they have been removed.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-manager@23550 423fd1db-d629-0410-8442-d21db03e70f4

--- src/manage_sqlite3.c
+++ src/manage_sqlite3.c
@@ -2607,6 +2607,7 @@ create_tables ()
 
   owned_clause = acl_where_owned_for_get ("override", "users.id");
 
+  sql_begin_exclusive ();
   sql ("DROP VIEW IF EXISTS result_overrides;");
   sql ("CREATE VIEW result_overrides AS"
        " SELECT users.id AS user,"
@@ -2636,9 +2637,11 @@ create_tables ()
        " overrides.port DESC, overrides.severity ASC,"
        " overrides.creation_time DESC",
        owned_clause);
+  sql ("COMMIT;");
 
   g_free (owned_clause);
 
+  sql_begin_exclusive ();
   sql ("DROP VIEW IF EXISTS result_new_severities;");
   sql ("CREATE VIEW result_new_severities AS"
        "  SELECT results.id as result, users.id as user, dynamic, override,"
@@ -2692,7 +2695,9 @@ create_tables ()
        "  FROM results, users"
        "  JOIN (SELECT 0 AS override UNION SELECT 1 AS override_opts)"
        "  JOIN (SELECT 0 AS dynamic UNION SELECT 1 AS dynamic_opts);");
+  sql ("COMMIT;");
 
+  sql_begin_exclusive ();
   sql ("DROP VIEW IF EXISTS results_autofp;");
   sql ("CREATE VIEW results_autofp AS"
        " SELECT results.id as result, autofp_selection,"
@@ -2755,6 +2760,7 @@ create_tables ()
        "  (SELECT 0 AS autofp_selection"
        "   UNION SELECT 1 AS autofp_selection"
        "   UNION SELECT 2 AS autofp_selection) AS autofp_opts;");
+  sql ("COMMIT;");
 }
 
 
