37ed171eee06589100e3a18c146f959bdcd2ae3b
Timo Pollmeier <timo.pollmeier@greenbone.net>
2015-10-16 08:32:36 +0000

    Backport r23601.
    	* src/manage_sql.c (delete_lsc_credential): Check if a trash target
    	uses the credential for ESXi when deleting it from trashcan.
    
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/branches/openvas-manager-6.0@23603 423fd1db-d629-0410-8442-d21db03e70f4

--- src/manage_sql.c
+++ src/manage_sql.c
@@ -34569,7 +34569,10 @@ delete_lsc_credential (const char *lsc_credential_id, int ultimate)
                    " WHERE (lsc_credential = %llu"
                    "        AND ssh_location = " G_STRINGIFY (LOCATION_TRASH) ")"
                    " OR (smb_lsc_credential = %llu"
-                   "     AND smb_location = " G_STRINGIFY (LOCATION_TRASH) ");",
+                   "     AND smb_location = " G_STRINGIFY (LOCATION_TRASH) ")"
+                   " OR (esxi_lsc_credential = %llu"
+                   "     AND esxi_location = " G_STRINGIFY (LOCATION_TRASH) ");",
+                   lsc_credential,
                    lsc_credential,
                    lsc_credential))
         {
