73c6b6874de98c9793f09939cee0e7ade6138122
Matthew Mundell <matthew.mundell@greenbone.net>
2016-04-07 09:24:12 +0000

    Backport r24759.
    	* src/omp.c (omp_xml_handle_end_element): In GET_ALERTS skip SCP
    	password.
    
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/branches/openvas-manager-6.0@24901 423fd1db-d629-0410-8442-d21db03e70f4

--- src/omp.c
+++ src/omp.c
@@ -12902,6 +12902,9 @@ omp_xml_handle_end_element (/*@unused@*/ GMarkupParseContext* context,
                 {
                   const char *name;
                   name = alert_data_iterator_name (&data);
+                  if (strcmp (alert_data_iterator_name (&data), "scp_password")
+                      == 0)
+                    continue;
                   if (strcmp (name, "notice") == 0)
                     notice = atoi (alert_data_iterator_data (&data));
                   else if (strcmp (method, "Email") == 0
