f0697ad475ac300bdcc76ff0c637b40ba6583c1e
Matthew Mundell <matthew.mundell@greenbone.net>
2016-05-05 09:18:26 +0000

    * src/manage.c (PERMISSION_ITERATOR_FILTER_COLUMNS): Add orphan. 	(PERMISSION_ITERATOR_COLUMNS): Name the orphan column.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-manager@25167 423fd1db-d629-0410-8442-d21db03e70f4

--- src/manage_sql.c
+++ src/manage_sql.c
@@ -51697,7 +51697,7 @@ trash_permission_writable (permission_t permission)
  */
 #define PERMISSION_ITERATOR_FILTER_COLUMNS                               \
  { GET_ITERATOR_FILTER_COLUMNS, "type", "resource_uuid", "subject_type", \
-   "_subject", "_resource", "subject_uuid", NULL }
+   "_subject", "_resource", "subject_uuid", "orphan", NULL }
 
 /**
  * @brief Permission iterator columns.
@@ -51719,7 +51719,7 @@ trash_permission_writable (permission_t permission)
    { "resource_location = " G_STRINGIFY (LOCATION_TRASH),                    \
      NULL,                                                                   \
      KEYWORD_TYPE_INTEGER },                                                 \
-   { "resource = -1", NULL, KEYWORD_TYPE_INTEGER },                          \
+   { "resource = -1", "orphan", KEYWORD_TYPE_INTEGER },                      \
    { "subject_type", NULL, KEYWORD_TYPE_STRING },                            \
    {                                                                         \
      "(CASE"                                                                 \
