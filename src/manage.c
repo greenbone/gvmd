cdad8aa0f29a3782e8fedb35e761a52613f51ba4
Matthew Mundell <matthew.mundell@greenbone.net>
2016-05-10 09:35:36 +0000

    Add alert method SNMP.
    	* src/manage.c (alert_method_name, alert_method_from_name): Add SNMP.
    
    	* src/manage.h (alert_method_t): Add SNMP.
    
    	* src/manage_sql.c (snmp_to_host): New function.
    	(escalate_2): Add SNMP.
    
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-manager@25207 423fd1db-d629-0410-8442-d21db03e70f4

--- src/manage.c
+++ src/manage.c
@@ -890,6 +890,7 @@ alert_method_name (alert_method_t method)
       case ALERT_METHOD_HTTP_GET:    return "HTTP Get";
       case ALERT_METHOD_SCP:         return "SCP";
       case ALERT_METHOD_SEND:        return "Send";
+      case ALERT_METHOD_SNMP:        return "SNMP";
       case ALERT_METHOD_SOURCEFIRE:  return "Sourcefire Connector";
       case ALERT_METHOD_START_TASK:  return "Start Task";
       case ALERT_METHOD_SYSLOG:      return "Syslog";
@@ -958,6 +959,8 @@ alert_method_from_name (const char* name)
     return ALERT_METHOD_SCP;
   if (strcasecmp (name, "Send") == 0)
     return ALERT_METHOD_SEND;
+  if (strcasecmp (name, "SNMP") == 0)
+    return ALERT_METHOD_SNMP;
   if (strcasecmp (name, "Sourcefire Connector") == 0)
     return ALERT_METHOD_SOURCEFIRE;
   if (strcasecmp (name, "Start Task") == 0)
