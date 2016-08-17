dc764a8fc28d5d4b13095e29d2bcb5faf25f1317
Hani Benhabiles <hani.benhabiles@greenbone.net>
2016-08-17 12:54:16 +0000

    * src/manage_sql.c (create_scanner): Handle unix socket case too.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-manager@26029 423fd1db-d629-0410-8442-d21db03e70f4

--- src/manage_sql.c
+++ src/manage_sql.c
@@ -44655,7 +44655,9 @@ create_scanner (const char* name, const char *comment, const char *host,
     return 2;
   if (itype <= SCANNER_TYPE_NONE || itype >= SCANNER_TYPE_MAX)
     return 2;
-  if (openvas_get_host_type (host) == -1)
+  /* XXX: Workaround for unix socket case. Should add a host type flag, or
+   * remove otp over tcp case entirely. */
+  if (openvas_get_host_type (host) == -1 && *host != '/')
     return 2;
   if (resource_with_name_exists (name, "scanner", 0))
     {
