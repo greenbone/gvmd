e482d4030adc68c70f3c737f77c5c5a88de5eb0d
Matthew Mundell <matthew.mundell@greenbone.net>
2017-06-28 12:57:32 +0000

    * src/sql_sqlite3.c (BUSY_TIMEOUT): New define. 	(sql_open, sql_prepare_internal, sql_exec_internal): Set busy timeout, to 	prevent process from waiting busily.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gvm@28847 423fd1db-d629-0410-8442-d21db03e70f4

--- src/sql_sqlite3.c
+++ src/sql_sqlite3.c
@@ -45,6 +45,11 @@
  */
 #define G_LOG_DOMAIN "md manage"
 
+/**
+ * @brief Busy timeout, in milliseconds.
+ */
+#define BUSY_TIMEOUT 1000
+
 
 /* Headers of sql.c symbols used only here. */
 
@@ -260,6 +265,8 @@ sql_open (const char *database)
       return -1;
     }
 
+  sqlite3_busy_timeout (task_db, BUSY_TIMEOUT);
+
   g_debug ("   %s: db open, max retry sleep time is %i\n",
            __FUNCTION__,
            GVM_SQLITE_SLEEP_MAX);
@@ -366,6 +373,9 @@ sql_prepare_internal (int retry, int log, const char* sql, va_list args,
   if (log)
     g_debug ("   sql: %s\n", formatted);
 
+  if (retry == 0)
+    sqlite3_busy_timeout (task_db, 0);
+
   retries = 0;
   *stmt = (sql_stmt_t*) g_malloc0 (sizeof (sql_stmt_t));
   sqlite_stmt = NULL;
@@ -386,6 +396,8 @@ sql_prepare_internal (int retry, int log, const char* sql, va_list args,
           if (retries++ < 10)
             continue;
           g_free (formatted);
+          if (retry == 0)
+            sqlite3_busy_timeout (task_db, BUSY_TIMEOUT);
           return 1;
         }
       g_free (formatted);
@@ -397,6 +409,8 @@ sql_prepare_internal (int retry, int log, const char* sql, va_list args,
               g_warning ("%s: sqlite3_prepare failed with NULL stmt: %s\n",
                          __FUNCTION__,
                          sqlite3_errmsg (task_db));
+              if (retry == 0)
+                sqlite3_busy_timeout (task_db, BUSY_TIMEOUT);
               return -1;
             }
           break;
@@ -404,9 +418,13 @@ sql_prepare_internal (int retry, int log, const char* sql, va_list args,
       g_warning ("%s: sqlite3_prepare failed: %s\n",
                  __FUNCTION__,
                  sqlite3_errmsg (task_db));
+      if (retry == 0)
+        sqlite3_busy_timeout (task_db, BUSY_TIMEOUT);
       return -1;
     }
 
+  if (retry == 0)
+    sqlite3_busy_timeout (task_db, BUSY_TIMEOUT);
   return 0;
 }
 
@@ -424,6 +442,9 @@ int
 sql_exec_internal (int retry, sql_stmt_t *stmt)
 {
   unsigned int retries;
+
+  if (retry == 0)
+    sqlite3_busy_timeout (task_db, 0);
 
   retries = 0;
   while (1)
@@ -444,6 +465,8 @@ sql_exec_internal (int retry, sql_stmt_t *stmt)
             continue;
           return -2;
         }
+      if (retry == 0)
+        sqlite3_busy_timeout (task_db, BUSY_TIMEOUT);
       if (ret == SQLITE_DONE)
         return 0;
       if (ret == SQLITE_ROW)
