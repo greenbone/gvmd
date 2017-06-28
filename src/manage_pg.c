2d75552a4609800d44023c3b4b768a0e23d2ee6f
Timo Pollmeier <timo.pollmeier@greenbone.net>
2017-06-28 14:29:35 +0000

    * src/manage_pg.c (manage_create_sql_functions): Test if the operator 	exists before trying to create it.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gvm@28856 423fd1db-d629-0410-8442-d21db03e70f4

--- src/manage_pg.c
+++ src/manage_pg.c
@@ -564,8 +564,13 @@ manage_create_sql_functions ()
        " LANGUAGE C;",
        GVM_LIB_INSTALL_DIR);
 
-  sql ("CREATE OPERATOR ?~#"
-       " (PROCEDURE = regexp, LEFTARG = text, RIGHTARG = text);");
+  if (sql_int ("SELECT count(*) FROM pg_operator"
+               " WHERE oprname = '?~#';")
+      == 0)
+    {
+      sql ("CREATE OPERATOR ?~#"
+          " (PROCEDURE = regexp, LEFTARG = text, RIGHTARG = text);");
+    }
 
   sql ("RESET role;");
 
