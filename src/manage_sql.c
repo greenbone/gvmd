d2dab7faa90e348064af2d84ab26170050690b24
Matthew Mundell <matthew.mundell@greenbone.net>
2017-08-14 14:57:34 +0000

    * src/manage_sql.c (copy_permission, delete_permission): Only check command 	level permissions for predefined roles.  This is consistent with the checks 	when creating and modifying permissions.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gvm@29348 423fd1db-d629-0410-8442-d21db03e70f4

--- src/manage_sql.c
+++ src/manage_sql.c
@@ -51991,10 +51991,11 @@ copy_permission (const char* comment, const char *permission_id,
       return 2;
     }
 
-  /* Check if the subject is a predefined role. */
+  /* Prevent copying of command level permissions for predefined roles. */
   subject_type = permission_subject_type (permission);
   subject = permission_subject (permission);
-  if (subject_type
+  if (permission_resource (permission) == 0
+      && subject_type
       && strcmp (subject_type, "role") == 0
       && subject
       && role_is_predefined (subject))
@@ -52722,10 +52723,12 @@ delete_permission (const char *permission_id, int ultimate)
       return 0;
     }
 
-  /* Check if the subject is a predefined role. */
+  /* Prevent deletion of command level permissions for predefined roles. */
   subject_type = permission_subject_type (permission);
   subject = permission_subject (permission);
-  if (subject_type
+  resource = permission_resource (permission);
+  if (resource == 0
+      && subject_type
       && strcmp (subject_type, "role") == 0
       && subject
       && role_is_predefined (subject))
@@ -52758,7 +52761,6 @@ delete_permission (const char *permission_id, int ultimate)
 
   name = permission_name (permission);
   resource_type = permission_resource_type (permission);
-  resource = permission_resource (permission);
 
   sql ("DELETE FROM permissions WHERE id = %llu;", permission);
 
