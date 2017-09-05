4c2da40b159dfc0573572ff0570feab8c4383e38
Timo Pollmeier <timo.pollmeier@greenbone.net>
2017-09-05 09:23:58 +0000

    Backport r29489.
    	Add SMB alert method and fix checks if alerts are using credentials
    	or report formats.
    
    	* CMakeLists.txt: Install new SMB alert script.
    
    	* src/omp.c (omp_xml_handle_end_element): In cases for MODIFY_ALERT and
    	CREATE_ALERT handle errors in event data and SMB method data.
    
    	* src/manage_sql.c (validate_smb_data): New function.
    	(create_alert, modify_alert): Add validation of SMB method data.
    	(smb_send_to_host, report_content_for_alert): New functions.
    	(escalate_2): Add case for SMB method.
    	(credential_in_use, trash_credential_in_use): Put alert method checks
    	into a single SQL query and add check for smb_credential.
    	(report_format_in_use, trash_report_format_in_use): Add checks for for
    	scp_report_format, send_report_format and smb_report_format.
    
    	* src/manage.c (alert_method_name, alert_method_from_name): Add new SMB
    	alert method.
    
    	* src/manage.h (alert_method_t): Add ALERT_METHOD_SMB.
    
    	* src/alert_methods/SMB/alert: New file.
    
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/branches/openvas-manager-7.0@29501 423fd1db-d629-0410-8442-d21db03e70f4

--- src/manage.c
+++ src/manage.c
@@ -1150,6 +1150,7 @@ alert_method_name (alert_method_t method)
       case ALERT_METHOD_HTTP_GET:    return "HTTP Get";
       case ALERT_METHOD_SCP:         return "SCP";
       case ALERT_METHOD_SEND:        return "Send";
+      case ALERT_METHOD_SMB:         return "SMB";
       case ALERT_METHOD_SNMP:        return "SNMP";
       case ALERT_METHOD_SOURCEFIRE:  return "Sourcefire Connector";
       case ALERT_METHOD_START_TASK:  return "Start Task";
@@ -1219,6 +1220,8 @@ alert_method_from_name (const char* name)
     return ALERT_METHOD_SCP;
   if (strcasecmp (name, "Send") == 0)
     return ALERT_METHOD_SEND;
+  if (strcasecmp (name, "SMB") == 0)
+    return ALERT_METHOD_SMB;
   if (strcasecmp (name, "SNMP") == 0)
     return ALERT_METHOD_SNMP;
   if (strcasecmp (name, "Sourcefire Connector") == 0)
