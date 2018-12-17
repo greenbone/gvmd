fbe9215bedeb5062e3638d0618ec32085252f5b0
Timo Pollmeier <timo.pollmeier@greenbone.net>
2018-12-17 16:49:14 +0100

    Use passphrase in check_private_key
    The function will now use gnutls_x509_privkey_import2 which will try to
    decrypt the private key when required.

--- src/omp.c
+++ src/omp.c
@@ -547,12 +547,13 @@ check_certificate (const char *cert_str)
 /**
  * @brief Check that a string represents a valid Private Key.
  *
- * @param[in]  key_str  Private Key string.
+ * @param[in]  key_str      Private Key string.
+ * @param[in]  key_phrase   Private Key passphrase.
  *
  * @return 0 if valid, 1 otherwise.
  */
 static int
-check_private_key (const char *key_str)
+check_private_key (const char *key_str, const char *key_phrase)
 {
   gnutls_x509_privkey_t key;
   gnutls_datum_t data;
@@ -563,7 +564,8 @@ check_private_key (const char *key_str)
     return 1;
   data.size = strlen (key_str);
   data.data = (void *) g_strdup (key_str);
-  ret = gnutls_x509_privkey_import (key, &data, GNUTLS_X509_FMT_PEM);
+  ret = gnutls_x509_privkey_import2 (key, &data, GNUTLS_X509_FMT_PEM,
+                                     key_phrase, 0);
   if (ret)
     {
       g_message ("%s: import failed: %s",
@@ -22424,7 +22426,8 @@ omp_xml_handle_end_element (/* unused */ GMarkupParseContext* context,
             }
           else if (create_credential_data->key
                    && create_credential_data->key_private == NULL
-                   && check_private_key (create_credential_data->key_private))
+                   && check_private_key (create_credential_data->key_private,
+                                         create_credential_data->key_phrase))
             {
               SEND_TO_CLIENT_OR_FAIL
               (XML_ERROR_SYNTAX ("create_credential",
