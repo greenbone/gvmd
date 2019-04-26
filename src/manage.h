d60eb5a83e396bf88f1cde1bb1b1846bbeb461f7
Timo Pollmeier <timo.pollmeier@greenbone.net>
2019-04-26 18:02:19 +0200

    Get correct name, UUID of task schedules in trash
    GET_TASKS previously queried the wrong table for the name and UUID of
    the schedule if it was in the trashcan.

--- src/manage.h
+++ src/manage.h
@@ -2882,7 +2882,13 @@ char *
 schedule_uuid (schedule_t);
 
 char *
+trash_schedule_uuid (schedule_t);
+
+char *
 schedule_name (schedule_t);
+
+char *
+trash_schedule_name (schedule_t);
 
 int
 schedule_duration (schedule_t);
