cd1d131a5a4ee901cf408f531775b1eda3f069dd
Matt Mundell <matthew.mundell@greenbone.net>
2019-06-04 14:07:18 +0200

    Add a more complex test, for gmp_tickets.c
    This adds a test for create_ticket_run, which is complex because
    create_ticket_run calls into other parts of the gvmd code (copy_ticket,
    ticket_uuid, log_event, etc).
    
    The test creates 'mock' versions of those functions.
    
    A gcc attribute is used as an easy way to override the mocked functions.
    The alternative would be to link against less of the gvmd code and to
    fill the gaps with stub functions.  But our code is not very cleanly
    separated to this is a lot of work.  Should get easier as we clean
    things up, if the attribute is not suitable.

--- src/tests.c
+++ src/tests.c
@@ -40,7 +40,7 @@ main (int argc, char **argv)
   add_suite (suite, manage_utils_tests ());
 
   if (argc > 1)
-    return run_single_test (suite, argv[1], create_text_reporter());
+    return run_single_test (suite, argv[1], create_text_reporter ());
 
   return run_test_suite (suite, create_text_reporter ());
 }
