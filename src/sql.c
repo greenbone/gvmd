ad134b8ca6747d05b19dce95512e9c842139d42b
Matt Mundell <matthew.mundell@greenbone.net>
2019-10-17 18:42:45 +0200

    Note restrictions of iterator_array

--- src/sql.c
+++ src/sql.c
@@ -656,6 +656,10 @@ iterator_string (iterator_t* iterator, int col)
 /**
  * @brief Get a string column from an iterator.
  *
+ * Note that sql_column_array gets the array as text and parses that text
+ * into an array, but it does not consider escaping so it probably will
+ * not work with strings that can contain commas, '{'s or '}'s.
+ *
  * @param[in]  iterator  Iterator.
  * @param[in]  col       Column offset.
  *
