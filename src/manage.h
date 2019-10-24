3dde3201e0dc003e3191f3d2d0ebaa2f420ebad1
Timo Pollmeier <timo.pollmeier@greenbone.net>
2019-10-24 12:06:12 +0200

    Add option --optimize migrate-relay-sensors
    If relays are active, this can be used to make sure all sensor type
    scanners have a matching relay, i.e. OSP sensors have an OSP relay
    and GMP scanners have an GMP relay.
    GMP scanners are migrated to OSP sensors if an OSP relay is available.

--- src/manage.h
+++ src/manage.h
@@ -2863,6 +2863,15 @@ void
 set_relay_mapper_path (const char *);
 
 int
+get_relay_migrate_sensors ();
+
+void
+set_relay_migrate_sensors (int);
+
+gboolean
+relay_supports_scanner_type (const char *, int, scanner_type_t);
+
+int
 slave_get_relay (const char *,
                  int,
                  const char *,
