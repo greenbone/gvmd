87523bf9991d50f079de49bd138658285912faf2
Matt Mundell <matthew.mundell@greenbone.net>
2019-11-04 09:32:08 +0200

    Remove unused SQL function next_time

--- src/manage_pg.c
+++ src/manage_pg.c
@@ -582,24 +582,6 @@ manage_create_sql_functions ()
        " LANGUAGE C;",
        GVM_LIB_INSTALL_DIR);
 
-  sql ("CREATE OR REPLACE FUNCTION next_time (integer, integer, integer, integer)"
-       " RETURNS integer"
-       " AS '%s/libgvm-pg-server', 'sql_next_time'"
-       " LANGUAGE C;",
-       GVM_LIB_INSTALL_DIR);
-
-  sql ("CREATE OR REPLACE FUNCTION next_time (integer, integer, integer, integer, text)"
-       " RETURNS integer"
-       " AS '%s/libgvm-pg-server', 'sql_next_time'"
-       " LANGUAGE C;",
-       GVM_LIB_INSTALL_DIR);
-
-  sql ("CREATE OR REPLACE FUNCTION next_time (integer, integer, integer, integer, text, integer)"
-       " RETURNS integer"
-       " AS '%s/libgvm-pg-server', 'sql_next_time'"
-       " LANGUAGE C;",
-       GVM_LIB_INSTALL_DIR);
-
   sql ("CREATE OR REPLACE FUNCTION next_time_ical (text, text)"
        " RETURNS integer"
        " AS '%s/libgvm-pg-server', 'sql_next_time_ical'"
