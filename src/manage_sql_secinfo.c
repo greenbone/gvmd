e408c73cbb6d3c3f9135a358c36a628210922225
Matt Mundell <matthew.mundell@greenbone.net>
2019-11-20 10:17:09 +0200

    Add SCAP migrator for unique constraint

--- src/manage_sql_secinfo.c
+++ src/manage_sql_secinfo.c
@@ -4203,6 +4203,12 @@ check_scap_db_version ()
        g_info ("Reinitialization of the database necessary");
        return manage_db_reinit ("scap");
        break;
+      case 15:
+       sql ("ALTER TABLE scap.affected_products ADD UNIQUE (cve, cpe);");
+       sql ("UPDATE scap.meta"
+            " SET value = '16'"
+            " WHERE name = 'database_version';");
+       break;
     }
   return 0;
 }
