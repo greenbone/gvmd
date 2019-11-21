7ad136e4028a57394448453ff50c82d3dcf8b2b2
Matt Mundell <matthew.mundell@greenbone.net>
2019-11-21 12:26:21 +0200

    Use right format specifier for merge_ovaldef version (9.0)
    'version' is a string, not an int.

--- src/manage_sql_secinfo.c
+++ src/manage_sql_secinfo.c
@@ -3065,7 +3065,7 @@ update_ovaldef_xml (gchar **file_and_date,
                   else
                     quoted_status = sql_quote ("");
 
-                  sql ("SELECT merge_ovaldef ('%s', '%s', '', %i, %i, %i, %i,"
+                  sql ("SELECT merge_ovaldef ('%s', '%s', '', %i, %i, %s, %i,"
                        "                      '%s', '%s', '%s', '%s', '%s',"
                        "                      %i);",
                        quoted_id,
