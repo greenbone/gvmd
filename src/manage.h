69acc69b67ad74268567905f8b36b6da0ae0cd7f
Matt Mundell <matthew.mundell@greenbone.net>
2020-02-14 13:27:22 +0200

    Move config headers to correct file
    manage_config.h now better matches manage_configs.  And gmp_configs.c no
    longer includes manage.h (directly or indirectly).

--- src/manage.h
+++ src/manage.h
@@ -26,6 +26,7 @@
 #define _GVMD_MANAGE_H
 
 #include "iterator.h"
+#include "manage_configs.h"
 #include "manage_get.h"
 
 #include <stdio.h>
@@ -307,7 +308,6 @@ typedef enum scanner_type
   SCANNER_TYPE_MAX,
 } scanner_type_t;
 
-typedef long long int config_t;
 typedef long long int credential_t;
 typedef long long int alert_t;
 typedef long long int filter_t;
@@ -1717,161 +1717,18 @@ int
 target_login_port (target_t, const char*);
 
 
-/* Configs. */
-
-/**
- * @brief An NVT selector.
- */
-typedef struct
-{
-  char *name;           ///< Name of NVT selector.
-  char *type;           ///< Name of NVT selector.
-  int include;          ///< Whether family/NVT is included or excluded.
-  char *family_or_nvt;  ///< Family or NVT that this selector selects.
-} nvt_selector_t;
-
-int
-create_config (const char*, const char*, int, const char*, const array_t*,
-               const array_t*, const char*, const char*, config_t*, char**);
-
-int
-create_config_from_scanner (const char*, const char *, const char *,
-                            const char *, char **);
-
-int
-copy_config (const char*, const char*, const char *, const char *, config_t*);
-
-int
-delete_config (const char*, int);
-
-int
-sync_config (const char *);
-
-gboolean
-find_config_with_permission (const char*, config_t*, const char *);
-
-char *
-config_uuid (config_t);
-
-int
-config_type (config_t);
-
-char *
-config_nvt_timeout (config_t, const char *);
-
-void
-init_user_config_iterator (iterator_t*, config_t, int, int, const char*);
-
-int
-init_config_iterator (iterator_t*, const get_data_t*);
-
-const char*
-config_iterator_nvt_selector (iterator_t*);
-
-int
-config_iterator_nvt_count (iterator_t*);
-
-int
-config_iterator_family_count (iterator_t*);
-
-int
-config_iterator_nvts_growing (iterator_t*);
-
-int
-config_iterator_type (iterator_t*);
-
-int
-config_iterator_families_growing (iterator_t*);
+/* Configs.
+ *
+ * These are here because they need definitions that are still in manage.h. */
 
 scanner_t
 config_iterator_scanner (iterator_t*);
-
-int
-config_iterator_scanner_trash (iterator_t*);
-
-const char*
-config_iterator_usage_type (iterator_t*);
-
-char*
-config_nvt_selector (config_t);
-
-int
-config_in_use (config_t);
-
-int
-config_writable (config_t);
-
-int
-config_count (const get_data_t *);
-
-int
-trash_config_in_use (config_t);
-
-int
-trash_config_writable (config_t);
-
-int
-trash_config_readable_uuid (const gchar *);
-
-int
-config_families_growing (config_t);
-
-int
-config_nvts_growing (config_t);
 
 int
 create_task_check_config_scanner (config_t, scanner_t);
 
 int
 modify_task_check_config_scanner (task_t, const char *, const char *);
-
-int
-manage_set_config_preference (const gchar *, const char*, const char*,
-                              const char*);
-
-void
-init_config_preference_iterator (iterator_t *, config_t);
-
-const char*
-config_preference_iterator_name (iterator_t *);
-
-const char*
-config_preference_iterator_value (iterator_t *);
-
-const char*
-config_preference_iterator_type (iterator_t *);
-
-const char*
-config_preference_iterator_default (iterator_t *);
-
-const char*
-config_preference_iterator_hr_name (iterator_t *);
-
-int
-manage_set_config (const gchar *, const char*, const char *, const char *);
-
-int
-manage_set_config_nvts (const gchar *, const char*, GPtrArray*);
-
-int
-manage_set_config_families (const gchar *, GPtrArray*, GPtrArray*, GPtrArray*,
-                            int);
-
-void
-init_config_timeout_iterator (iterator_t*, config_t);
-
-const char*
-config_timeout_iterator_oid (iterator_t *);
-
-const char*
-config_timeout_iterator_nvt_name (iterator_t *);
-
-const char*
-config_timeout_iterator_value (iterator_t *);
-
-void
-update_config_preference (const char *, const char *, const char *,
-                          const char *, gboolean);
 
 
 /* NVT's. */
