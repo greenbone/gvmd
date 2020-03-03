16fdadf175230a7fe32e229bc6d8d7cd5e3fb8c2
Matt Mundell <matthew.mundell@greenbone.net>
2020-03-03 16:29:53 +0200

    Implement --rebuild

--- src/manage_sql_nvts.c
+++ src/manage_sql_nvts.c
@@ -1749,9 +1749,12 @@ update_or_rebuild (int update)
 
   osp_connection_close (connection);
 
+  if (update == 0)
+    sql ("TRUNCATE nvts;");
+
   if (update_nvt_cache_osp (osp_update_socket, NULL, scanner_feed_version))
     {
-      printf ("Failed to update NVT cache.\n");
+      printf ("Failed to %s NVT cache.\n", update ? "update" : "rebuild);
       return -1;
     }
 
@@ -1778,9 +1781,7 @@ manage_rebuild (GSList *log_config, const gchar *database)
   if (ret)
     return ret;
 
-  // FIX
-
-  current_credentials.uuid = NULL;
+  ret = update_or_rebuild (0);
 
   manage_option_cleanup ();
 
