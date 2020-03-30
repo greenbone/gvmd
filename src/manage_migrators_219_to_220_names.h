159d12592b360a0134e7482e4b88d0d7488c0979
Matt Mundell <matthew.mundell@greenbone.net>
2020-03-30 16:40:30 +0200

    Update .h SPDX-License-Identifier

--- src/manage_migrators_219_to_220_names.h
+++ src/manage_migrators_219_to_220_names.h
@@ -1,6 +1,6 @@
-/* Copyright (C) 2019 Greenbone Networks GmbH
+/* Copyright (C) 2018 Greenbone Networks GmbH
  *
- * SPDX-License-Identifier: GPL-2.0-or-later
+ * SPDX-License-Identifier: AGPL-3.0-or-later
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
