4a302dd29285e77aefa89a466b22e6a395d5a9fe
Timo Pollmeier <timo.pollmeier@greenbone.net>
2020-04-17 15:33:00 +0200

    Fix return code handling in (manage_)rebuild_scap
    The rebuild_scap now handles the return code from open_secinfo_lockfile
    and unused failure return codes have been removed from
    manage_rebuild_scap and its documentation.

--- src/manage_sql_secinfo.c
+++ src/manage_sql_secinfo.c
@@ -4485,6 +4485,10 @@ rebuild_scap (const char *type)
   int lockfile;
 
   ret = open_secinfo_lockfile ("gvm-sync-scap", &lockfile);
+  if (ret == 1)
+    return 2;
+  else if (ret)
+    return -1;
 
   if (strcasecmp (type, "ovaldefs") == 0
       || strcasecmp (type, "ovaldef") == 0)
@@ -4522,9 +4526,7 @@ rebuild_scap (const char *type)
  * @param[in]  database    Location of manage database.
  * @param[in]  type        The type of SCAP info to rebuild.
 
- * @return 0 success, -1 error, -2 main database is wrong version,
- *         -3 main database needs to be initialised from server,
- *         -5 sync currently running.
+ * @return 0 success, -1 error.
  */
 int
 manage_rebuild_scap (GSList *log_config, const gchar *database,
@@ -4536,7 +4538,7 @@ manage_rebuild_scap (GSList *log_config, const gchar *database,
 
   ret = manage_option_setup (log_config, database);
   if (ret)
-    return ret;
+    return -1;
 
   if (manage_update_scap_db_init ())
     goto fail;
@@ -4561,6 +4563,11 @@ manage_rebuild_scap (GSList *log_config, const gchar *database,
   if (ret == 1)
     {
       printf ("Type must be 'ovaldefs'.\n");
+      goto fail;
+    }
+  else if (ret == 2)
+    {
+      printf ("SCAP sync is currently running.\n");
       goto fail;
     }
   else if (ret)
