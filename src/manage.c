be338564e84b3d62f812cc7bd51a2b0eba920084
ArnoStiefvater <arno.stiefvater@greenbone.net>
2020-04-21 12:09:06 +0200

    fix typo
    Because of typos some scanner_options did not get inserted correctly
    when starting an ospd task.

--- src/manage.c
+++ src/manage.c
@@ -4236,12 +4236,12 @@ launch_osp_openvas_task (task_t task, target_t target, const char *scan_id,
 
   source_iface = task_preference_value (task, "source_iface");
   if (source_iface)
-    g_hash_table_insert (scanner_options, g_strdup ("source_ifae"),
+    g_hash_table_insert (scanner_options, g_strdup ("source_iface"),
                         source_iface);
 
   hosts_ordering = task_hosts_ordering (task);
   if (hosts_ordering)
-    g_hash_table_insert (scanner_options, g_strdup ("host_ordering"),
+    g_hash_table_insert (scanner_options, g_strdup ("hosts_ordering"),
                          hosts_ordering);
 
   /* Setup reverse_lookup_only preference. */
