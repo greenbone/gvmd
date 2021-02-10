09f6a48e7a0c7aaf6b90dd111d54fb8b065ed97b
Timo Pollmeier <timo.pollmeier@greenbone.net>
2021-02-10 15:44:33 +0100

    Init delta_states, min_qod in print_report_xml_start
    (cherry picked from commit 25c51e74e480fd84928047a27e3433a260e938f9)

--- src/manage_sql.c
+++ src/manage_sql.c
@@ -27021,6 +27021,8 @@ print_report_xml_start (report_t report, report_t delta, task_t task,
   max_results = -1;
   levels = NULL;
   zone = NULL;
+  delta_states = NULL;
+  min_qod = NULL;
   total_result_count = filtered_result_count = 0;
   orig_filtered_result_count = 0;
   orig_f_false_positives = orig_f_warnings = orig_f_logs = orig_f_infos = 0;
