f6435ac899113fc23cfefab47d345ed4b8391154
Timo Pollmeier <timo.pollmeier@greenbone.net>
2021-04-06 15:53:34 +0200

    Simplify feed_lockfile_lock_timeout
    The feed_lockfile_lock function is used to avoid duplicating the code to
    write the timestamp. Also, the return value checks are clarified.
    
    (cherry picked from commit 3c2dc47ff0210698a67edac470a08114762eee77)

--- src/manage.c
+++ src/manage.c
@@ -8647,7 +8647,7 @@ feed_lockfile_lock (lockfile_t *lockfile)
 int
 feed_lockfile_lock_timeout (lockfile_t *lockfile)
 {
-  int ret;
+  int lock_status;
   gboolean log_timeout;
   time_t timeout_end;
 
@@ -8657,8 +8657,9 @@ feed_lockfile_lock_timeout (lockfile_t *lockfile)
   timeout_end = time (NULL) + feed_lock_timeout;
   do
     {
-      ret = lockfile_lock_path_nb (lockfile, get_feed_lock_path ());
-      if (ret == 1 && timeout_end > time (NULL))
+      lock_status = feed_lockfile_lock (lockfile);
+      if (lock_status == 1 /* already locked, but no error */
+          && timeout_end > time (NULL))
         {
           if (log_timeout)
             {
@@ -8669,14 +8670,11 @@ feed_lockfile_lock_timeout (lockfile_t *lockfile)
             }
           gvm_sleep (1);
         }
-      else if (ret)
+      else if (lock_status) /* error */
         {
-          return ret;
+          return lock_status;
         }
-    } while (ret);
-
-  /* Write the file contents (timestamp) */
-  write_sync_start (lockfile->fd);
+    } while (lock_status); /* lock is acquired when lock_status is 0 */
 
   return 0;
 }
