a0f1574540b0faa6ed55838bd8970098f0551ed2
Philipp Eder <philipp.eder@greenbone.net>
2021-05-07 15:45:01 +0200

    Move doxygen comments from header into implementation
    (cherry picked from commit 6a406e815d3f1f64a6d28d2cd2795bb6abb89a0a)

--- src/manage_authentication.c
+++ src/manage_authentication.c
@@ -8,6 +8,30 @@
 // rather than having to check and fail.
 struct PBASettings settings = {{0}, COUNT_DEFAULT, PREFIX_DEFAULT};
 
+/**
+ * @brief manage_authentication_setup sets the pepper, count and prefix used
+ * by the authentication implementation.
+ *
+ * When pepper is a NULL pointer, prefix is a NULL pointer or count is 0 then
+ * the previous setting of that setting will be kept.
+ *
+ * This is mainly to allow easier configuration within gvmd.c so that it can
+ * used parameterized without restore the same information there.
+ *
+ * The initial defaults are set to no-pepper, COUNT_DEFAULT and PREFIX_DEFAULT
+ * of gvm-libs.
+ *
+ * @param[in] pepper - a static hidden addition to the randomely generated salt
+ * @param[in] pepper_size - the size of pepper; it must not larger then
+ * MAX_PEPPER_SIZE
+ * @param[in] count - the amount of rounds used to calculate the hash; if 0 then
+ * COUNT_DEFAULT will be used
+ * @param[in] prefix - the used algorithm, if NULL pointer then the most secure
+ * available algorithm will be used.
+ *
+ * @return GMA_SUCCESS when the settings are set or GMA_ERR if there was a
+ * failure.
+ */
 enum manage_authentication_rc
 manage_authentication_setup (const char *pepper, unsigned int pepper_size,
                              unsigned int count, char *prefix)
@@ -31,12 +55,31 @@ exit:
   return rc;
 }
 
+/**
+ * @brief creates a hash based on the settings set by
+ * manage_authentication_setup and the password.
+ *
+ * @param[in] passwod - the password to be hashed
+ * @return the hash or a NULL pointer on a failure.
+ * */
 char *
 manage_authentication_hash (const char *password)
 {
     return pba_hash(&settings, password);
 }
 
+/**
+ * @brief manage_authentication_verify verifies given password with given hash.
+ *
+ * @param[in] password - the clear text password to be verified
+ * @param[in] hash - the stored hash to verify the password against.
+ *
+ * @return GMA_SUCCESS when password is valid,
+ *          GMA_HASH_VALID_BUT_DATED when password is valid but a new hash
+ *          should ne created and stored.
+ *          GMA_HASH_INVALID when password is invalid
+ *          GMA_ERR when an unexpected error occurs.
+ **/
 enum manage_authentication_rc
 manage_authentication_verify (const char *hash, const char *password)
 {
