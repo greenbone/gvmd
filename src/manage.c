c9e5d28e4a1a604278e84bba5603228e745a2579
Johannes Helmold <johannes.helmold@greenbone.net>
2021-06-01 13:57:54 +0200

    Changed the functioncall of free(..) to osp_credential_free(..)
    in manage.c: Changed the call of free(osp_credential) to
      osp_credential_free(osp_credential) in function
      target_osp_ssh_credential(..).

--- src/manage.c
+++ src/manage.c
@@ -2135,7 +2135,7 @@ target_osp_ssh_credential (target_t target)
             {
               g_warning ("%s: SSH Elevate Credential not found.", __func__);
               cleanup_iterator (&ssh_elevate_iter);
-              free(osp_credential);
+              osp_credential_free(osp_credential);
               return NULL;
             }
           elevate_type = credential_iterator_type (&ssh_elevate_iter);
@@ -2143,7 +2143,7 @@ target_osp_ssh_credential (target_t target)
             {
               g_warning ("%s: SSH Elevate Credential not of type up", __func__);
               cleanup_iterator (&ssh_elevate_iter);
-              free(osp_credential);
+              osp_credential_free(osp_credential);
               return NULL;
             }
           osp_credential_set_auth_data (osp_credential,
