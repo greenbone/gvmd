a54b278fec5e0a97f202fd4657d3c8df18e48c8b
Johannes Helmold <johannes.helmold@greenbone.net>
2021-07-19 12:52:59 +0200

    Amended the setting of the value "connection_retry".
    In manage.c, in function get_osp_performance_string (..):
      Replaced the assignment of value "connection_retry =
      SCANNER_CONNECTION_RETRY_DEFAULT;" by
      "connection_retry = get_scanner_connection_retry();".
    
    (cherry picked from commit 3fa72180d0df7b91791678a445ae70df5e0edd22)

--- src/manage.c
+++ src/manage.c
@@ -6201,7 +6201,7 @@ get_osp_performance_string (scanner_t scanner, int start, int end,
   key_pub = scanner_key_pub (scanner);
   key_priv = scanner_key_priv (scanner);
 
-  connection_retry = SCANNER_CONNECTION_RETRY_DEFAULT;
+  connection_retry = get_scanner_connection_retry();
   while(connection == NULL && connection_retry > 0)
     {
       connection = osp_connect_with_data (host, port,
@@ -6223,7 +6223,7 @@ get_osp_performance_string (scanner_t scanner, int start, int end,
   opts.titles = g_strdup (titles);
   error = NULL;
 
-  connection_retry = SCANNER_CONNECTION_RETRY_DEFAULT;
+  connection_retry = get_scanner_connection_retry();
   return_value = 1;
   while (return_value > 0 && connection_retry > 0)
     {
