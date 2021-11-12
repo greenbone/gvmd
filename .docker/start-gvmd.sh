b33f5043d8a0b8f2a2c17fe099acc6a930c8be1e
Björn Ricks <bjoern.ricks@greenbone.net>
2021-11-12 14:41:49 +0100

    Various commits, to improve docker images and startup script
    Allow to set different gvmd arguments via environment variable
    
    Use a dedicated run directory
    
    Only one single service should use a runtime directory. Therefore use a
    dedicated one for gvmd.
    
    Use a dedicated usename in the container
    
    For consistency use a dedicated user name for the gvmd container.
    
    Switch back to /run/gvm as directory
    
    Changing the run directory is only possible when building gvm-libs
    because the pid files are created via gvm-libs. Therefore changing the
    run dir in gvmd will only change it for the unix socket.
    
    Ensure that variables in start-gvmd script are quoted
    
    Avoid issues with shell programming and always quote input variables.
    
    Build gvmd image correctly by using our action
    
    Currently the main branch is not uplaoded as unstable. Therefore change
    to using our container-image-tags action.
    
    Use correct new gvmd user when testing if postgres is available

--- .docker/start-gvmd.sh
+++ .docker/start-gvmd.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-[ -z $USER ] && USER="admin"
-[ -z $PASSWORD ] && PASSWORD="admin"
+
+[ -z "$USER" ] && USER="admin"
+[ -z "$PASSWORD" ] && PASSWORD="admin"
+[ -z "$GVMD_ARGS" ] && GVMD_ARGS="--listen-mode 666"
+[ -z "$GVMD_USER" ] && GVMD_USER="gvmd"
 
 # check for psql connection
-until psql -U gvm -d gvmd -c "SELECT 'connected' as connection"; do
+until psql -U "$GVMD_USER" -d gvmd -c "SELECT 'connected' as connection"; do
 	echo "waiting 1 second to retry psql connection"
 	sleep 1
 done
@@ -18,7 +21,7 @@ uid=$(gvmd --get-users --verbose | grep $USER | awk '{print $2}')
 gvmd --modify-setting 78eceaec-3385-11ea-b237-28d24461215b --value "$uid"
 
 echo "starting gvmd"
-gvmd --listen-mode 666 ||
+gvmd "$GVMD_ARGS" ||
 	(cat /var/log/gvm/gvmd.log && exit 1)
 
 tail -f /var/log/gvm/gvmd.log
