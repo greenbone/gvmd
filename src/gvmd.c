d1f38908e26f4bb136a978a1c4adac636389a94f
Johannes Helmold <johannes.helmold@greenbone.net>
2022-01-26 11:26:23 +0100

    Removed the "#ifndef NDEBUG" to get a backtrace in gvmd-journal.
    The backtrace functionality in gvmd.c was, because of the
    surrounding "#ifndef NDEBUG" statement, only activated on
    development systems. Now that statement is removed to also
    get the backtrace (eventually listed in the gvmd-journal)
    on GOS-systems of the customer.
    
    (cherry picked from commit a02850169c5a53b734335beaa2f54cfb43b18d53)

--- src/gvmd.c
+++ src/gvmd.c
@@ -928,7 +928,6 @@ cleanup ()
   if (is_parent == 1) pidfile_remove (GVMD_PID_PATH);
 }
 
-#ifndef NDEBUG
 #include <execinfo.h>
 
 /**
@@ -937,7 +936,6 @@ cleanup ()
  * For debugging backtrace in \ref handle_sigabrt.
  */
 #define BA_SIZE 100
-#endif
 
 /**
  * @brief Handle a SIGABRT signal.
@@ -952,7 +950,6 @@ handle_sigabrt (int given_signal)
   if (in_sigabrt) _exit (EXIT_FAILURE);
   in_sigabrt = 1;
 
-#ifndef NDEBUG
   void *frames[BA_SIZE];
   int frame_count, index;
   char **frames_text;
@@ -968,7 +965,6 @@ handle_sigabrt (int given_signal)
   for (index = 0; index < frame_count; index++)
     g_debug ("BACKTRACE: %s", frames_text[index]);
   free (frames_text);
-#endif
 
   manage_cleanup_process_error (given_signal);
   cleanup ();
@@ -998,7 +994,6 @@ handle_termination_signal (int signal)
 static void
 handle_sigsegv (/* unused */ int given_signal)
 {
-#ifndef NDEBUG
   void *frames[BA_SIZE];
   int frame_count, index;
   char **frames_text;
@@ -1014,7 +1009,6 @@ handle_sigsegv (/* unused */ int given_signal)
   for (index = 0; index < frame_count; index++)
     g_debug ("BACKTRACE: %s", frames_text[index]);
   free (frames_text);
-#endif
 
   manage_cleanup_process_error (given_signal);
 
