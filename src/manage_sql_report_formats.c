48f77b52497b1063cc1be8f6edf9546fb207ed1b
Björn Ricks <bjoern.ricks@greenbone.net>
2022-10-10 11:14:48 +0200

    Change: Escape minus in regular expressions for pcre 2 compatibility
    Escape all minus symbols in regular expressions to ensure the
    expressions are compatible with pcre 2.
    
    (cherry picked from commit 912350f7c40e47b24c121d7d1a27a2ec5bcd9754)

--- src/manage_sql_report_formats.c
+++ src/manage_sql_report_formats.c
@@ -2472,7 +2472,7 @@ validate_param_value (report_format_t report_format,
       case REPORT_FORMAT_PARAM_TYPE_REPORT_FORMAT_LIST:
         {
           if (g_regex_match_simple
-                ("^(?:[[:alnum:]-_]+)?(?:,(?:[[:alnum:]-_])+)*$", value, 0, 0)
+                ("^(?:[[:alnum:]\\-_]+)?(?:,(?:[[:alnum:]\\-_])+)*$", value, 0, 0)
               == FALSE)
             return 1;
           else
