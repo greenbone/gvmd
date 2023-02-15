49dfd26ddba7b96c425d7ef4b3de5c9ee8750b19
Johannes Helmold <johannes.helmold@greenbone.net>
2023-02-15 10:06:43 +0100

    Small addition.

--- src/manage_sql.c
+++ src/manage_sql.c
@@ -53363,6 +53363,7 @@ manage_set_radius_info (int enabled, gchar *host, gchar *key)
            quoted);
       g_free (secret);
       g_free (quoted);
+      lsc_crypt_release(crypt_ctx);
     }
 
   sql_commit ();
