ac406a3ad5c9b100b382909bba0d92f2ab411aa6
Timo Pollmeier <timo.pollmeier@greenbone.net>
2023-04-28 09:21:10 +0200

    Fix: Fix result detection for imported reports
    When importing reports, the "detected_at" report host details generated
    from result detection details now use the VT ID of the result.
    This makes it so the result detection now works correctly in the
    imported reports.
    
    (cherry picked from commit ea1ba5f3a18594c078d2edecb1adf7ad8bb83e6a)

--- src/gmp.c
+++ src/gmp.c
@@ -18095,8 +18095,7 @@ gmp_xml_handle_result ()
           detail->ip = g_strdup (result->host);
           detail->name = g_strdup ("detected_at");
           detail->source_desc = g_strdup ("create_report_import");
-          detail->source_name = g_strdup (
-            detection->source_oid); // verify when detected_at || detected_by
+          detail->source_name = g_strdup (result->nvt_oid);
           detail->source_type = g_strdup ("create_report_import");
           detail->value = g_strdup (detection->location);
           array_add (create_report_data->details, detail);
