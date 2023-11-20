d18e6c1c8dd3c31692cf28d0536840a5188ac2a0
Matt Mundell <matt@mundell.me>
2023-11-20 14:06:59 +0200

    Change: move GET iterator time conversion out of SQL

--- src/manage_sql.h
+++ src/manage_sql.h
@@ -248,9 +248,9 @@ typedef struct
  *
  * @param[in]  prefix  Column prefix.
  */
-#define GET_ITERATOR_COLUMNS_STRING                                \
-  "id, uuid, name, comment, iso_time (creation_time),"             \
-  " iso_time (modification_time), creation_time AS created,"       \
+#define GET_ITERATOR_COLUMNS_STRING                     \
+  "id, uuid, name, comment, creation_time,"             \
+  " modification_time, creation_time AS created,"       \
   " modification_time AS modified"
 
 /**
@@ -263,8 +263,8 @@ typedef struct
   { prefix "uuid", NULL, KEYWORD_TYPE_STRING },                             \
   { prefix "name", NULL, KEYWORD_TYPE_STRING },                             \
   { prefix "comment", NULL, KEYWORD_TYPE_STRING },                          \
-  { " iso_time (" prefix "creation_time)", NULL, KEYWORD_TYPE_STRING },     \
-  { " iso_time (" prefix "modification_time)", NULL, KEYWORD_TYPE_STRING }, \
+  { prefix "creation_time", NULL, KEYWORD_TYPE_INTEGER },                   \
+  { prefix "modification_time", NULL, KEYWORD_TYPE_INTEGER },               \
   { prefix "creation_time", "created", KEYWORD_TYPE_INTEGER },              \
   { prefix "modification_time", "modified", KEYWORD_TYPE_INTEGER }
 
