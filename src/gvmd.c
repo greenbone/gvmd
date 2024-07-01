7e20d1edf4a06490f7144aadc336a7018d413924
Juan Jose Nicola <juan.nicola@greenbone.net>
2024-07-01 15:02:56 -0300

    Add: functions to handle an Openvasd scan task

--- src/gvmd.c
+++ src/gvmd.c
@@ -2833,6 +2833,8 @@ gvmd (int argc, char** argv, char *env[])
         type = SCANNER_TYPE_OPENVAS;
       else if (!strcasecmp (scanner_type, "OSP-Sensor"))
         type = SCANNER_TYPE_OSP_SENSOR;
+      else if (!strcasecmp (scanner_type, "Openvasd"))
+        type = SCANNER_TYPE_OPENVASD;
       else
         {
           type = atoi (scanner_type);
@@ -2875,6 +2877,8 @@ gvmd (int argc, char** argv, char *env[])
             type = SCANNER_TYPE_OPENVAS;
           else if (!strcasecmp (scanner_type, "OSP-Sensor"))
             type = SCANNER_TYPE_OSP_SENSOR;
+          else if (!strcasecmp (scanner_type, "Openvasd"))
+            type = SCANNER_TYPE_OPENVASD;
           else
             {
               type = atoi (scanner_type);
