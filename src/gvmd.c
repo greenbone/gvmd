0871e61f485280a7a4d07e1c9082d5dd76a90227
Ahmed Abdelsalam <ahmed.abdelsalam@greenbone.net>
2025-02-05 16:05:21 +0100

    Update copyright and GMP feed version

--- src/gvmd.c
+++ src/gvmd.c
@@ -2333,7 +2333,7 @@ gvmd (int argc, char** argv, char *env[])
 #if CVSS3_RATINGS == 1
       printf ("CVSS3 severity ratings enabled\n");
 #endif
-      printf ("Copyright (C) 2009-2021 Greenbone AG\n");
+      printf ("Copyright (C) 2009-2025 Greenbone AG\n");
       printf ("License: AGPL-3.0-or-later\n");
       printf
         ("This is free software: you are free to change and redistribute it.\n"
