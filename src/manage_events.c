b1af30d3813531020aa8fb096d59d75e03e4a097
Matt Mundell <matt@mundell.me>
2025-03-25 15:32:42 +0200

    Change: move events to dedicated file

--- src/manage_events.c
+++ src/manage_events.c
@@ -31,3 +31,93 @@
  * @brief GLib log domain.
  */
 #define G_LOG_DOMAIN "md manage"
+
+/**
+ * @brief Get the name of an alert event.
+ *
+ * @param[in]  event  Event.
+ *
+ * @return The name of the event (for example, "Run status changed").
+ */
+const char*
+event_name (event_t event)
+{
+  switch (event)
+    {
+      case EVENT_TASK_RUN_STATUS_CHANGED: return "Task run status changed";
+      case EVENT_NEW_SECINFO:             return "New SecInfo arrived";
+      case EVENT_UPDATED_SECINFO:         return "Updated SecInfo arrived";
+      case EVENT_TICKET_RECEIVED:         return "Ticket received";
+      case EVENT_ASSIGNED_TICKET_CHANGED: return "Assigned ticket changed";
+      case EVENT_OWNED_TICKET_CHANGED:    return "Owned ticket changed";
+      default:                            return "Internal Error";
+    }
+}
+
+/**
+ * @brief Get a description of an alert event.
+ *
+ * @param[in]  event       Event.
+ * @param[in]  event_data  Event data.
+ * @param[in]  task_name   Name of task if required in description, else NULL.
+ *
+ * @return Freshly allocated description of event.
+ */
+gchar*
+event_description (event_t event, const void *event_data, const char *task_name)
+{
+  switch (event)
+    {
+      case EVENT_TASK_RUN_STATUS_CHANGED:
+        if (task_name)
+          return g_strdup_printf
+                  ("The security scan task '%s' changed status to '%s'",
+                   task_name,
+                   run_status_name ((task_status_t) event_data));
+        return g_strdup_printf ("Task status changed to '%s'",
+                                run_status_name ((task_status_t) event_data));
+        break;
+      case EVENT_NEW_SECINFO:
+        return g_strdup_printf ("New SecInfo arrived");
+        break;
+      case EVENT_UPDATED_SECINFO:
+        return g_strdup_printf ("Updated SecInfo arrived");
+        break;
+      case EVENT_TICKET_RECEIVED:
+        return g_strdup_printf ("Ticket received");
+        break;
+      case EVENT_ASSIGNED_TICKET_CHANGED:
+        return g_strdup_printf ("Assigned ticket changed");
+        break;
+      case EVENT_OWNED_TICKET_CHANGED:
+        return g_strdup_printf ("Owned ticket changed");
+        break;
+      default:
+        return g_strdup ("Internal Error");
+    }
+}
+
+/**
+ * @brief Get an event from a name.
+ *
+ * @param[in]  name  Event name.
+ *
+ * @return The event.
+ */
+event_t
+event_from_name (const char* name)
+{
+  if (strcasecmp (name, "Task run status changed") == 0)
+    return EVENT_TASK_RUN_STATUS_CHANGED;
+  if (strcasecmp (name, "New SecInfo arrived") == 0)
+    return EVENT_NEW_SECINFO;
+  if (strcasecmp (name, "Updated SecInfo arrived") == 0)
+    return EVENT_UPDATED_SECINFO;
+  if (strcasecmp (name, "Ticket received") == 0)
+    return EVENT_TICKET_RECEIVED;
+  if (strcasecmp (name, "Assigned ticket changed") == 0)
+    return EVENT_ASSIGNED_TICKET_CHANGED;
+  if (strcasecmp (name, "Owned ticket changed") == 0)
+    return EVENT_OWNED_TICKET_CHANGED;
+  return EVENT_ERROR;
+}
--- src/manage_events.h
+++ src/manage_events.h
@@ -19,4 +19,29 @@
 #ifndef _GVMD_MANAGE_EVENTS_H
 #define _GVMD_MANAGE_EVENTS_H
 
+#include "iterator.h"
+
+/**
+ * @brief Types of task events.
+ */
+typedef enum
+{
+  EVENT_ERROR,
+  EVENT_TASK_RUN_STATUS_CHANGED,
+  EVENT_NEW_SECINFO,
+  EVENT_UPDATED_SECINFO,
+  EVENT_TICKET_RECEIVED,
+  EVENT_ASSIGNED_TICKET_CHANGED,
+  EVENT_OWNED_TICKET_CHANGED
+} event_t;
+
+const char*
+event_name (event_t);
+
+gchar*
+event_description (event_t, const void *, const char *);
+
+event_t
+event_from_name (const char*);
+
 #endif /* not _GVMD_MANAGE_EVENTS_H */
