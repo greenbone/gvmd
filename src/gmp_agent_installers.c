3b7cfafb0f714f51355a6f5f1d42484fa8a40937
Timo Pollmeier <timo.pollmeier@greenbone.net>
2025-05-06 12:17:12 +0200

    Get last_update + CPEs in agent installer details

--- src/gmp_agent_installers.c
+++ src/gmp_agent_installers.c
@@ -142,6 +142,7 @@ get_agent_installers_run (gmp_parser_t *gmp_parser, GError **error)
   SEND_GET_START ("agent_installer");
   while (1)
     {
+      time_t last_update;
       ret = get_next (&agent_installers, &get_agent_installers_data.get, &first,
                       &count, init_agent_installer_iterator);
       if (ret == 1)
@@ -173,9 +174,15 @@ get_agent_installers_run (gmp_parser_t *gmp_parser, GError **error)
         agent_installer_iterator_file_size (&agent_installers)
       );
 
+      last_update 
+        = agent_installer_iterator_last_update (&agent_installers);
+      SENDF_TO_CLIENT_OR_FAIL ("<last_update>%s</last_update>",
+                               iso_if_time (last_update));
+      
       if (get_agent_installers_data.get.details)
         {
           gchar *file_validity;
+          iterator_t cpes_iterator;
           
           agent_installer_file_is_valid (
               agent_installer_iterator_installer_path (&agent_installers),
@@ -189,6 +196,61 @@ get_agent_installers_run (gmp_parser_t *gmp_parser, GError **error)
             file_validity
           );
           g_free (file_validity);
+
+          init_agent_installer_cpe_iterator (
+            &cpes_iterator,
+            get_iterator_resource (&agent_installers),
+            get_agent_installers_data.get.trash);
+
+          SENDF_TO_CLIENT_OR_FAIL ("<cpes>");
+          while (next (&cpes_iterator))
+            {
+              const char *version_start_incl, *version_start_excl;
+              const char *version_end_incl, *version_end_excl;
+
+              version_start_incl
+                = agent_installer_cpe_iterator_version_start_incl (
+                    &cpes_iterator);
+              version_start_excl
+                = agent_installer_cpe_iterator_version_start_excl (
+                    &cpes_iterator);
+              version_end_incl
+                = agent_installer_cpe_iterator_version_end_incl (
+                    &cpes_iterator);
+              version_end_excl
+                = agent_installer_cpe_iterator_version_end_excl (
+                                    &cpes_iterator);
+
+              SENDF_TO_CLIENT_OR_FAIL (
+                "<cpe>"
+                "<criteria>%s</criteria>",
+                agent_installer_cpe_iterator_criteria (&cpes_iterator)
+              );
+              
+              if (version_start_incl)
+                SENDF_TO_CLIENT_OR_FAIL (
+                  "<version_start_incl>%s</version_start_incl>",
+                  version_start_incl);
+
+              if (version_start_excl)
+                SENDF_TO_CLIENT_OR_FAIL (
+                  "<version_start_excl>%s</version_start_excl>",
+                  version_start_excl);
+
+              if (version_end_incl)
+                SENDF_TO_CLIENT_OR_FAIL (
+                  "<version_end_incl>%s</version_end_incl>",
+                  version_end_incl);
+
+              if (version_end_excl)
+                SENDF_TO_CLIENT_OR_FAIL (
+                  "<version_end_excl>%s</version_end_excl>",
+                  version_end_excl);
+
+              SENDF_TO_CLIENT_OR_FAIL ("</cpe>");
+            }
+          SENDF_TO_CLIENT_OR_FAIL ("</cpes>");
+          cleanup_iterator (&cpes_iterator);
         }
 
       SENDF_TO_CLIENT_OR_FAIL ("</agent_installer>");
