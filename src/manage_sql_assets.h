cbd39aa4887bd3b48cc13e019757f01be7aff299
Matt Mundell <matt@mundell.me>
2025-07-30 17:43:25 +0200

    Change: add dedicated .c and .h files for asset code

---
+++ src/manage_sql_assets.h
@@ -0,0 +1,34 @@
+/* Copyright (C) 2009-2025 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _GVMD_MANAGE_SQL_ASSETS_H
+#define _GVMD_MANAGE_SQL_ASSETS_H
+
+#include "manage_resources.h"
+
+/**
+ * @file
+ * @brief GVM management layer: Asset SQL
+ *
+ * The Asset SQL for the GVM management layer.
+ */
+
+char *
+result_host_asset_id (const char *, result_t);
+
+#endif /* not _GVMD_MANAGE_SQL_ASSETS_H */
