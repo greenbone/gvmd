2ff506e531d8fb33411ec73cdc53df2fa9cbe272
ozgen <ozgn45@gmail.com>
2025-07-31 07:59:50 +0200

    Fix typo in modify_task result

--- src/gmp.c
+++ src/gmp.c
@@ -27157,7 +27157,7 @@ gmp_xml_handle_end_element (/* unused */ GMarkupParseContext* context,
                     error_send_to_client (error);
                     return;
                   }
-                log_event_fail ("agent_group", "Agent Group",
+                log_event_fail ("task", "Task",
                                 modify_task_data->task_id, "modified");
                 break;
 #endif /*ENABLE_AGENTS*/
