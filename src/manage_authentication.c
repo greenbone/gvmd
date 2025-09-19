ee79c3b5758288966534baa3be7d12c5668c799d
Matt Mundell <matt@mundell.me>
2025-09-19 17:13:24 +0200

    Doc: add @file to auth

--- src/manage_authentication.c
+++ src/manage_authentication.c
@@ -16,6 +16,13 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * @file
+ * @brief GVM management layer: authentication.
+ *
+ * General authentication functions.
+ */
+
 #include "manage_authentication.h"
 #include <gvm/util/passwordbasedauthentication.h>
 
