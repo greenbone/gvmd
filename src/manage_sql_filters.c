2249c606ed441d85338e8cad6e8174695e3344c4
Matt Mundell <matt@mundell.me>
2025-09-22 19:56:18 +0200

    Change: add manage_sql_filters.c

---
+++ src/manage_sql_filters.c
@@ -0,0 +1,32 @@
+/* Copyright (C) 2019-2025 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "manage_filters.h"
+
+#undef G_LOG_DOMAIN
+/**
+ * @brief GLib log domain.
+ */
+#define G_LOG_DOMAIN "md manage"
+
+/**
+ * @file
+ * @brief GVM management layer: Filters SQL
+ *
+ * The Filters SQL for the GVM management layer.
+ */
