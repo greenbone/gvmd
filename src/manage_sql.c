4f15c4eef7544878816c3e6936a5f0a3b3a658dc
Matt Mundell <matt@mundell.me>
2026-01-06 11:26:04 +0200

    Change: move copy_role to dedicated role files

--- src/manage_sql.c
+++ src/manage_sql.c
@@ -35119,64 +35119,6 @@ clean_feed_role_permissions (const char *type,
 /* Roles. */
 
 /**
- * @brief Create a role from an existing role.
- *
- * @param[in]  name       Name of new role.  NULL to copy from existing.
- * @param[in]  comment    Comment on new role.  NULL to copy from existing.
- * @param[in]  role_id    UUID of existing role.
- * @param[out] new_role_return  New role.
- *
- * @return 0 success, 1 role exists already, 2 failed to find existing
- *         role, 99 permission denied, -1 error.
- */
-int
-copy_role (const char *name, const char *comment, const char *role_id,
-           role_t *new_role_return)
-{
-  int ret;
-  role_t new_role, old_role;
-
-  sql_begin_immediate ();
-
-  if (acl_user_may ("create_role") == 0)
-    return 99;
-
-  if (acl_role_can_super_everyone (role_id))
-    return 99;
-
-  ret = copy_resource_lock ("role", name, comment, role_id, NULL, 1, &new_role,
-                            &old_role);
-  if (ret)
-    {
-      sql_rollback ();
-      return ret;
-    }
-
-  sql ("INSERT INTO permissions"
-       " (uuid, owner, name, comment, resource_type, resource_uuid, resource,"
-       "  resource_location, subject_type, subject, subject_location,"
-       "  creation_time, modification_time)"
-       " SELECT make_uuid (),"
-       "        (SELECT id FROM users WHERE users.uuid = '%s'),"
-       "        name, comment, resource_type,"
-       "        resource_uuid, resource, resource_location, subject_type, %llu,"
-       "        subject_location, m_now (), m_now ()"
-       " FROM permissions"
-       " WHERE subject_type = 'role'"
-       " AND subject = %llu"
-       " AND subject_location = " G_STRINGIFY (LOCATION_TABLE)
-       " AND (resource = 0 OR owner IS NULL);",
-       current_credentials.uuid,
-       new_role,
-       old_role);
-
-  sql_commit ();
-  if (new_role_return)
-    *new_role_return = new_role;
-  return 0;
-}
-
-/**
  * @brief Create a role.
  *
  * @param[in]   role_name        Role name.
