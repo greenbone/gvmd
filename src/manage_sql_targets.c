b64a716cdd8f5462d07aba33a64932b4af79d778
Matt Mundell <matt@mundell.me>
2026-02-10 20:03:44 +0200

    Change: move target_uuid and trash_target_uuid to dedicated files

--- src/manage_sql_targets.c
+++ src/manage_sql_targets.c
@@ -4,6 +4,7 @@
  */
 
 #include "manage_sql_targets.h"
+#include "sql.h"
 
 /**
  * @file
@@ -12,3 +13,30 @@
  * The Targets SQL for the GVM management layer.
  */
 
+/**
+ * @brief Return the UUID of a target.
+ *
+ * @param[in]  target  Target.
+ *
+ * @return Newly allocated UUID if available, else NULL.
+ */
+char*
+target_uuid (target_t target)
+{
+  return sql_string ("SELECT uuid FROM targets WHERE id = %llu;",
+                     target);
+}
+
+/**
+ * @brief Return the UUID of a trashcan target.
+ *
+ * @param[in]  target  Target.
+ *
+ * @return Newly allocated UUID if available, else NULL.
+ */
+char*
+trash_target_uuid (target_t target)
+{
+  return sql_string ("SELECT uuid FROM targets_trash WHERE id = %llu;",
+                     target);
+}
--- src/manage_targets.h
+++ src/manage_targets.h
@@ -6,10 +6,18 @@
 #ifndef _GVMD_MANAGE_TARGETS_H
 #define _GVMD_MANAGE_TARGETS_H
 
+#include "manage_resources.h"
+
 int
 manage_max_hosts ();
 
 void
 manage_set_max_hosts (int);
+
+char*
+target_uuid (target_t);
+
+char*
+trash_target_uuid (target_t);
 
 #endif /* not _GVMD_MANAGE_TARGETS_H */
