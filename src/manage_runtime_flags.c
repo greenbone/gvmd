5325aba3dd318bcd1b23321ba36a7cabe5cd2129
ozgen mehmet <mehmet.oezgen@greenbone.net>
2026-03-03 11:29:05 +0100

    Add: add enable_osi_export feature flag (#2807)
    * Add: add enable_report_export feature flag
    
    Extend the existing feature flag mechanism by introducing
    `enable_report_export` to control report exporting at runtime.
    
    This allows enabling or disabling report exporting without
    requiring a rebuild.
    
    * Rename the feature flag to better match the OSI export naming.

--- src/manage_runtime_flags.c
+++ src/manage_runtime_flags.c
@@ -96,6 +96,12 @@ static feature_state_t feature_vt_metadata =
   {1, 0};
 
 /**
+ * @brief State of a single feature.
+ */
+static feature_state_t feature_osi_export =
+  {1, 0};
+
+/**
  * @brief Parse a textual boolean value into an integer.
  *
  * Recognized true values: "1", "true", "yes", "on"
@@ -194,6 +200,9 @@ struct conf_feature_flags
 
   int has_vt_metadata;         ///< Whether flag is present.
   int vt_metadata;             ///< Value of flag.
+
+  int has_osi_export;         ///< Whether flag is present.
+  int osi_export;             ///< Value of flag.
 };
 
 /**
@@ -292,6 +301,9 @@ load_conf_file_feature_flags (const char *config_path,
 
   load_feature_flag (kf, "features", "enable_vt_metadata",
                      &out->has_vt_metadata, &out->vt_metadata);
+
+  load_feature_flag (kf, "features", "enable_osi_export",
+                     &out->has_osi_export, &out->osi_export);
 
   g_key_file_unref (kf);
   return 0;
@@ -412,6 +424,11 @@ runtime_flags_init (const gchar *config_path)
                    conf_flags.has_vt_metadata,
                    conf_flags.vt_metadata);
 
+  resolve_feature (&feature_osi_export,
+                   "GVMD_ENABLE_OSI_EXPORT",
+                   conf_flags.has_osi_export,
+                   conf_flags.osi_export);
+
   return 0;
 }
 
@@ -441,6 +458,8 @@ feature_enabled (feature_id_t t)
       return feature_credential_stores.enabled;
     case FEATURE_ID_VT_METADATA:
       return feature_vt_metadata.enabled;
+    case FEATURE_ID_OSI_EXPORT:
+      return feature_osi_export.enabled;
     default:
       return 0;
     }
@@ -468,6 +487,8 @@ feature_compiled_in (feature_id_t t)
       return feature_credential_stores.compiled_in;
     case FEATURE_ID_VT_METADATA:
       return feature_vt_metadata.compiled_in;
+    case FEATURE_ID_OSI_EXPORT:
+      return feature_osi_export.compiled_in;
     default:
       return 0;
     }
