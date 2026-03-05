60e8e6feb9bc98d83425fee7da2d248e2c983c7e
Matt Mundell <matt@mundell.me>
2026-03-05 20:02:19 +0200

    Change: move tag_uuid to dedicated files

---
+++ src/manage_sql_tags.h
@@ -0,0 +1,11 @@
+/* Copyright (C) 2026 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#ifndef _GVMD_MANAGE_SQL_TAGS_H
+#define _GVMD_MANAGE_SQL_TAGS_H
+
+#include "manage_tags.h"
+
+#endif // not _GVMD_MANAGE_SQL_TAGS_H
