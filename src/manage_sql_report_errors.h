9f9fc0f5b2cfb4f000b04e4aa5ca82b69de2786a
ozgen <ozgn45@gmail.com>
2026-04-10 13:30:56 +0200

    change: move report errors iterator helpers to management layer
    Move the report errors iterator declarations from
    manage_sql_report_errors to manage_report_errors.
    
    These helpers are now used from the management layer, so
    placing them there makes ownership clearer and keeps the SQL
    module focused on SQL-specific report error handling.
    
    No functional changes intended.

--- src/manage_sql_report_errors.h
+++ src/manage_sql_report_errors.h
@@ -15,42 +15,6 @@
 
 #include "manage_sql.h"
 
-void
-init_report_errors_iterator (iterator_t *, report_t);
-
-const char *
-report_errors_iterator_host (iterator_t *);
-
-const char *
-report_errors_iterator_severity (iterator_t *);
-
-const char *
-report_errors_iterator_scan_nvt_version (iterator_t *);
-
-const char *
-report_errors_iterator_port (iterator_t *);
-
-const char *
-report_errors_iterator_nvt_oid (iterator_t *);
-
-const char *
-report_errors_iterator_desc (iterator_t *);
-
-const char *
-report_errors_iterator_nvt_name (iterator_t *);
-
-const char *
-report_errors_iterator_nvt_cvss (iterator_t *);
-
-const char *
-report_errors_iterator_scan_nvt_version (iterator_t *);
-
-const char *
-report_errors_iterator_severity (iterator_t *);
-
-int
-report_error_count (report_t);
-
 int
 print_report_errors_xml (report_t, FILE *);
 
