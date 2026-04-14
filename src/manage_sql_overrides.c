b0f423ab3240e2cb9f54d79a1123d35378c00ab6
Matt Mundell <matt@mundell.me>
2026-04-14 17:30:45 +0200

    Change: move override iterators to dedicated files

--- src/manage_sql_overrides.c
+++ src/manage_sql_overrides.c
@@ -5,6 +5,7 @@
 
 #include "manage_sql_overrides.h"
 #include "manage_acl.h"
+#include "manage_sql_filters.h"
 #include "manage_sql_permissions.h"
 #include "manage_sql_resources.h"
 #include "manage_sql_settings.h"
@@ -702,3 +703,472 @@ modify_override (const gchar *override_id, const char *active, const char *nvt,
 
   return 0;
 }
+
+/**
+ * @brief Count number of overrides.
+ *
+ * @param[in]  get         GET params.
+ * @param[in]  result      Result to limit overrides to, 0 for all.
+ * @param[in]  task        If result is > 0, task whose overrides on result to
+ *                         include, otherwise task to limit overrides to.  0 for
+ *                         all tasks.
+ * @param[in]  nvt         NVT to limit overrides to, 0 for all.
+ *
+ * @return Total number of overrides in filtered set.
+ */
+int
+override_count (const get_data_t *get, nvt_t nvt, result_t result, task_t task)
+{
+  static const char *filter_columns[] = OVERRIDE_ITERATOR_FILTER_COLUMNS;
+  static column_t columns[] = OVERRIDE_ITERATOR_COLUMNS;
+  static column_t trash_columns[] = OVERRIDE_ITERATOR_TRASH_COLUMNS;
+  gchar *result_clause, *filter, *task_id;
+  int ret;
+
+  /* Treat the "task_id" filter keyword as if the task was given in "task". */
+
+  if (get->filt_id && strcmp (get->filt_id, FILT_ID_NONE))
+    {
+      filter = filter_term (get->filt_id);
+      if (filter == NULL)
+        return 2;
+    }
+  else
+    filter = NULL;
+
+  task_id = filter_term_value (filter ? filter : get->filter, "task_id");
+
+  g_free (filter);
+
+  if (task_id)
+    {
+      find_task_with_permission (task_id, &task, "get_tasks");
+      g_free (task_id);
+    }
+
+  if (result)
+    {
+      gchar *severity_sql;
+
+      if (setting_dynamic_severity_int ())
+        severity_sql = g_strdup_printf ("(SELECT CASE"
+                                        " WHEN results.severity"
+                                        "      > " G_STRINGIFY (SEVERITY_LOG)
+                                        " THEN CAST (nvts.cvss_base AS real)"
+                                        " ELSE results.severity END"
+                                        " FROM results, nvts"
+                                        " WHERE (nvts.oid = results.nvt)"
+                                        "   AND (results.id = %llu))",
+                                        result);
+      else
+        severity_sql = g_strdup_printf ("(SELECT results.severity"
+                                        " FROM results"
+                                        " WHERE results.id = %llu)",
+                                        result);
+
+      result_clause = g_strdup_printf (" AND"
+                                       " (result = %llu"
+                                       "  OR (result = 0 AND nvt ="
+                                       "      (SELECT results.nvt FROM results"
+                                       "       WHERE results.id = %llu)))"
+                                       " AND (hosts is NULL"
+                                       "      OR hosts = ''"
+                                       "      OR hosts_contains (hosts,"
+                                       "      (SELECT results.host FROM results"
+                                       "       WHERE results.id = %llu)))"
+                                       " AND (port is NULL"
+                                       "      OR port = ''"
+                                       "      OR port ="
+                                       "      (SELECT results.port FROM results"
+                                       "       WHERE results.id = %llu))"
+                                       " AND (severity_matches_ov (%s,"
+                                       "                           severity))"
+                                       " AND (task = 0 OR task = %llu)",
+                                       result,
+                                       result,
+                                       result,
+                                       result,
+                                       severity_sql,
+                                       task);
+
+      g_free (severity_sql);
+    }
+  else if (task)
+    {
+      result_clause = g_strdup_printf
+                       (" AND (overrides.task = %llu OR overrides.task = 0)"
+                        " AND nvt IN"
+                        " (SELECT DISTINCT nvt FROM results"
+                        "  WHERE results.task = %llu)"
+                        " AND (overrides.result = 0"
+                        "      OR (SELECT task FROM results"
+                        "          WHERE results.id = overrides.result)"
+                        "         = %llu)",
+                        task,
+                        task,
+                        task);
+    }
+  else if (nvt)
+    {
+      result_clause = g_strdup_printf
+                       (" AND (overrides.nvt"
+                        "      = (SELECT oid FROM nvts WHERE nvts.id = %llu))",
+                        nvt);
+    }
+  else
+    result_clause = NULL;
+
+  ret = count ("override",
+               get,
+               columns,
+               trash_columns,
+               filter_columns,
+               task || nvt,
+               NULL,
+               result_clause,
+               TRUE);
+
+  g_free (result_clause);
+
+  return ret;
+}
+
+/**
+ * @brief Initialise an override iterator.
+ *
+ * @param[in]  iterator    Iterator.
+ * @param[in]  get         GET data.
+ * @param[in]  result      Result to limit overrides to, 0 for all.
+ * @param[in]  task        If result is > 0, task whose overrides on result to
+ *                         include, otherwise task to limit overrides to.  0 for
+ *                         all tasks.
+ * @param[in]  nvt         NVT to limit overrides to, 0 for all.
+ *
+ * @return 0 success, 1 failed to find target, 2 failed to find filter,
+ *         -1 error.
+ */
+int
+init_override_iterator (iterator_t* iterator, const get_data_t *get, nvt_t nvt,
+                        result_t result, task_t task)
+{
+  static const char *filter_columns[] = OVERRIDE_ITERATOR_FILTER_COLUMNS;
+  static column_t columns[] = OVERRIDE_ITERATOR_COLUMNS;
+  static column_t trash_columns[] = OVERRIDE_ITERATOR_TRASH_COLUMNS;
+  gchar *result_clause, *filter, *task_id;
+  int ret;
+
+  assert (current_credentials.uuid);
+  assert ((nvt && get->id) == 0);
+  assert ((task && get->id) == 0);
+
+  assert (result ? nvt == 0 : 1);
+  assert (task ? nvt == 0 : 1);
+
+  /* Treat the "task_id" filter keyword as if the task was given in "task". */
+
+  if (get->filt_id && strcmp (get->filt_id, FILT_ID_NONE))
+    {
+      filter = filter_term (get->filt_id);
+      if (filter == NULL)
+        return 2;
+    }
+  else
+    filter = NULL;
+
+  task_id = filter_term_value (filter ? filter : get->filter, "task_id");
+
+  g_free (filter);
+
+  if (task_id)
+    {
+      find_task_with_permission (task_id, &task, "get_tasks");
+      g_free (task_id);
+    }
+
+  if (result)
+    {
+      gchar *severity_sql;
+
+      if (setting_dynamic_severity_int ())
+        severity_sql = g_strdup_printf ("(SELECT CASE"
+                                        " WHEN results.severity"
+                                        "      > " G_STRINGIFY (SEVERITY_LOG)
+                                        " THEN CAST (nvts.cvss_base AS real)"
+                                        " ELSE results.severity END"
+                                        " FROM results, nvts"
+                                        " WHERE (nvts.oid = results.nvt)"
+                                        "   AND (results.id = %llu))",
+                                        result);
+      else
+        severity_sql = g_strdup_printf ("(SELECT results.severity"
+                                        " FROM results"
+                                        " WHERE results.id = %llu)",
+                                        result);
+
+      result_clause = g_strdup_printf (" AND"
+                                       " (result = %llu"
+                                       "  OR (result = 0 AND nvt ="
+                                       "      (SELECT results.nvt FROM results"
+                                       "       WHERE results.id = %llu)))"
+                                       " AND (hosts is NULL"
+                                       "      OR hosts = ''"
+                                       "      OR hosts_contains (hosts,"
+                                       "      (SELECT results.host FROM results"
+                                       "       WHERE results.id = %llu)))"
+                                       " AND (port is NULL"
+                                       "      OR port = ''"
+                                       "      OR port ="
+                                       "      (SELECT results.port FROM results"
+                                       "       WHERE results.id = %llu))"
+                                       " AND (severity_matches_ov (%s,"
+                                       "                           severity))"
+                                       " AND (task = 0 OR task = %llu)",
+                                       result,
+                                       result,
+                                       result,
+                                       result,
+                                       severity_sql,
+                                       task);
+
+      g_free (severity_sql);
+    }
+  else if (task)
+    {
+      result_clause = g_strdup_printf
+                       (" AND (overrides.task = %llu OR overrides.task = 0)"
+                        " AND nvt IN"
+                        " (SELECT DISTINCT nvt FROM results"
+                        "  WHERE results.task = %llu)"
+                        " AND (overrides.result = 0"
+                        "      OR (SELECT task FROM results"
+                        "          WHERE results.id = overrides.result)"
+                        "         = %llu)",
+                        task,
+                        task,
+                        task);
+    }
+  else if (nvt)
+    {
+      result_clause = g_strdup_printf
+                       (" AND (overrides.nvt = (SELECT oid FROM nvts"
+                       "                        WHERE nvts.id = %llu))",
+                        nvt);
+    }
+  else
+    result_clause = NULL;
+
+  ret = init_get_iterator (iterator,
+                           "override",
+                           get,
+                           columns,
+                           trash_columns,
+                           filter_columns,
+                           task || nvt,
+                           NULL,
+                           result_clause,
+                           TRUE);
+
+  g_free (result_clause);
+
+  return ret;
+}
+
+/**
+ * @brief Initialise an override iterator not limited to result, task or NVT.
+ *
+ * @param[in]  iterator    Iterator.
+ * @param[in]  get         GET data.
+ *
+ * @return 0 success, 1 failed to find target, 2 failed to find filter,
+ *         -1 error.
+ */
+int
+init_override_iterator_all (iterator_t* iterator, get_data_t *get)
+{
+  return init_override_iterator (iterator, get, 0, 0, 0);
+}
+
+/**
+ * @brief Get the NVT OID from a override iterator.
+ *
+ * @param[in]  iterator  Iterator.
+ *
+ * @return NVT OID, or NULL if iteration is complete.  Freed by
+ *         cleanup_iterator.
+ */
+DEF_ACCESS (override_iterator_nvt_oid, GET_ITERATOR_COLUMN_COUNT);
+
+/**
+ * @brief Get the text from a override iterator.
+ *
+ * @param[in]  iterator  Iterator.
+ *
+ * @return Text, or NULL if iteration is complete.  Freed by
+ *         cleanup_iterator.
+ */
+DEF_ACCESS (override_iterator_text, GET_ITERATOR_COLUMN_COUNT + 1);
+
+/**
+ * @brief Get the hosts from a override iterator.
+ *
+ * @param[in]  iterator  Iterator.
+ *
+ * @return Hosts, or NULL if iteration is complete.  Freed by
+ *         cleanup_iterator.
+ */
+DEF_ACCESS (override_iterator_hosts, GET_ITERATOR_COLUMN_COUNT + 2);
+
+/**
+ * @brief Get the port from a override iterator.
+ *
+ * @param[in]  iterator  Iterator.
+ *
+ * @return Port, or NULL if iteration is complete.  Freed by
+ *         cleanup_iterator.
+ */
+DEF_ACCESS (override_iterator_port, GET_ITERATOR_COLUMN_COUNT + 3);
+
+/**
+ * @brief Get the threat from a override iterator.
+ *
+ * @param[in]  iterator  Iterator.
+ *
+ * @return Threat.
+ */
+const char *
+override_iterator_threat (iterator_t *iterator)
+{
+  const char *ret;
+  if (iterator->done) return NULL;
+  ret = iterator_string (iterator, GET_ITERATOR_COLUMN_COUNT + 4);
+  return ret;
+}
+
+/**
+ * @brief Get the threat from an override iterator.
+ *
+ * @param[in]  iterator  Iterator.
+ *
+ * @return Threat.
+ */
+const char *
+override_iterator_new_threat (iterator_t *iterator)
+{
+  const char *ret;
+  if (iterator->done) return NULL;
+  ret = iterator_string (iterator, GET_ITERATOR_COLUMN_COUNT + 5);
+  return ret;
+}
+
+/**
+ * @brief Get the task from a override iterator.
+ *
+ * @param[in]  iterator  Iterator.
+ *
+ * @return The task associated with the override, or 0 on error.
+ */
+task_t
+override_iterator_task (iterator_t* iterator)
+{
+  if (iterator->done) return 0;
+  return (task_t) iterator_int64 (iterator, GET_ITERATOR_COLUMN_COUNT + 6);
+}
+
+/**
+ * @brief Get the result from a override iterator.
+ *
+ * @param[in]  iterator  Iterator.
+ *
+ * @return The result associated with the override, or 0 on error.
+ */
+result_t
+override_iterator_result (iterator_t* iterator)
+{
+  if (iterator->done) return 0;
+  return (result_t) iterator_int64 (iterator, GET_ITERATOR_COLUMN_COUNT + 7);
+}
+
+/**
+ * @brief Get the end time from an override iterator.
+ *
+ * @param[in]  iterator  Iterator.
+ *
+ * @return Time until which override applies.  0 for always.  1 means the
+ *         override has been explicitly turned off.
+ */
+time_t
+override_iterator_end_time (iterator_t* iterator)
+{
+  int ret;
+  if (iterator->done) return -1;
+  ret = (time_t) iterator_int (iterator, GET_ITERATOR_COLUMN_COUNT + 8);
+  return ret;
+}
+
+/**
+ * @brief Get the active status from an override iterator.
+ *
+ * @param[in]  iterator  Iterator.
+ *
+ * @return 1 if active, else 0.
+ */
+int
+override_iterator_active (iterator_t* iterator)
+{
+  int ret;
+  if (iterator->done) return -1;
+  ret = iterator_int (iterator, GET_ITERATOR_COLUMN_COUNT + 9);
+  return ret;
+}
+
+/**
+ * @brief Get the NVT name from a override iterator.
+ *
+ * @param[in]  iterator  Iterator.
+ *
+ * @return NVT name, or NULL if iteration is complete.  Freed by
+ *         cleanup_iterator.
+ */
+DEF_ACCESS (override_iterator_nvt_name, GET_ITERATOR_COLUMN_COUNT + 10);
+
+/**
+ * @brief Get the NVT type from a override iterator.
+ *
+ * @param[in]  iterator  Iterator.
+ *
+ * @return NVT type, or NULL.  Static string.
+ */
+const char *
+override_iterator_nvt_type (iterator_t *iterator)
+{
+  const char *oid;
+
+  oid = override_iterator_nvt_oid (iterator);
+  if (oid == NULL)
+    return NULL;
+
+  if (g_str_has_prefix (oid, "CVE-"))
+    return "cve";
+
+  return "nvt";
+}
+
+/**
+ * @brief Get the severity from an override iterator.
+ *
+ * @param[in]  iterator  Iterator.
+ *
+ * @return The severity score to which the override applies or NULL if
+ *         iteration is complete, Freed by cleanup_iterator.
+ */
+DEF_ACCESS (override_iterator_severity, GET_ITERATOR_COLUMN_COUNT + 14);
+
+/**
+ * @brief Get the new severity from an override iterator.
+ *
+ * @param[in]  iterator  Iterator.
+ *
+ * @return The severity score to override to or NULL if
+ *         iteration is complete, Freed by cleanup_iterator.
+ */
+DEF_ACCESS (override_iterator_new_severity, GET_ITERATOR_COLUMN_COUNT + 15);
--- src/manage_sql_overrides.h
+++ src/manage_sql_overrides.h
@@ -8,4 +8,160 @@
 
 #include "manage_overrides.h"
 
+/**
+ * @brief Filter columns for override iterator.
+ */
+#define OVERRIDE_ITERATOR_FILTER_COLUMNS                                      \
+ { ANON_GET_ITERATOR_FILTER_COLUMNS, "name", "nvt", "text", "nvt_id",         \
+   "task_name", "task_id", "hosts", "port", "threat", "new_threat", "active", \
+   "result", "severity", "new_severity", "active_days", NULL }
+
+/**
+ * @brief Override iterator columns.
+ */
+#define OVERRIDE_ITERATOR_COLUMNS                                           \
+ {                                                                          \
+   { "overrides.id", "id", KEYWORD_TYPE_INTEGER },                          \
+   { "overrides.uuid", "uuid", KEYWORD_TYPE_STRING },                       \
+   {                                                                        \
+     "(CASE"                                                                \
+     " WHEN overrides.nvt LIKE 'CVE-%%'"                                    \
+     " THEN overrides.nvt"                                                  \
+     " ELSE (SELECT name FROM nvts WHERE oid = overrides.nvt)"              \
+     " END)",                                                               \
+     "name",                                                                \
+     KEYWORD_TYPE_STRING                                                    \
+   },                                                                       \
+   { "CAST ('' AS TEXT)", NULL, KEYWORD_TYPE_STRING },                      \
+   { "overrides.creation_time", NULL, KEYWORD_TYPE_INTEGER },               \
+   { "overrides.modification_time", NULL, KEYWORD_TYPE_INTEGER },           \
+   { "overrides.creation_time", "created", KEYWORD_TYPE_INTEGER },          \
+   { "overrides.modification_time", "modified", KEYWORD_TYPE_INTEGER },     \
+   {                                                                        \
+     "(SELECT name FROM users WHERE users.id = overrides.owner)",           \
+     "_owner",                                                              \
+     KEYWORD_TYPE_STRING                                                    \
+   },                                                                       \
+   { "owner", NULL, KEYWORD_TYPE_INTEGER },                                 \
+   /* Columns specific to overrides. */                                     \
+   { "overrides.nvt", "oid", KEYWORD_TYPE_STRING },                         \
+   { "overrides.text", "text", KEYWORD_TYPE_STRING },                       \
+   { "overrides.hosts", "hosts", KEYWORD_TYPE_STRING },                     \
+   { "overrides.port", "port", KEYWORD_TYPE_STRING },                       \
+   { "severity_to_level (overrides.severity, 1)",                           \
+     "threat",                                                              \
+     KEYWORD_TYPE_STRING },                                                 \
+   { "severity_to_level (overrides.new_severity, 0)",                       \
+     "new_threat",                                                          \
+     KEYWORD_TYPE_STRING },                                                 \
+   { "overrides.task", NULL, KEYWORD_TYPE_STRING },                         \
+   { "overrides.result", "result", KEYWORD_TYPE_INTEGER },                  \
+   { "overrides.end_time", NULL, KEYWORD_TYPE_INTEGER },                    \
+   {                                                                        \
+     "CAST (((overrides.end_time = 0) OR (overrides.end_time >= m_now ()))" \
+     "      AS INTEGER)",                                                   \
+     "active",                                                              \
+     KEYWORD_TYPE_INTEGER                                                   \
+   },                                                                       \
+   {                                                                        \
+     "(CASE"                                                                \
+     " WHEN overrides.nvt LIKE 'CVE-%%'"                                    \
+     " THEN overrides.nvt"                                                  \
+     " ELSE (SELECT name FROM nvts WHERE oid = overrides.nvt)"              \
+     " END)",                                                               \
+     "nvt",                                                                 \
+     KEYWORD_TYPE_STRING                                                    \
+   },                                                                       \
+   { "overrides.nvt", "nvt_id", KEYWORD_TYPE_STRING },                      \
+   { "(SELECT uuid FROM tasks WHERE id = overrides.task)",                  \
+     "task_id",                                                             \
+     KEYWORD_TYPE_STRING },                                                 \
+   { "(SELECT name FROM tasks WHERE id = overrides.task)",                  \
+     "task_name",                                                           \
+     KEYWORD_TYPE_STRING },                                                 \
+   { "overrides.severity", "severity", KEYWORD_TYPE_DOUBLE },               \
+   { "overrides.new_severity", "new_severity", KEYWORD_TYPE_DOUBLE },       \
+   {                                                                        \
+     "(SELECT name FROM users WHERE users.id = overrides.owner)",           \
+     "_owner",                                                              \
+     KEYWORD_TYPE_STRING                                                    \
+   },                                                                       \
+   { "days_from_now (overrides.end_time)",                                  \
+     "active_days",                                                         \
+     KEYWORD_TYPE_INTEGER },                                                \
+   { NULL, NULL, KEYWORD_TYPE_UNKNOWN }                                     \
+ }
+
+/**
+ * @brief Override iterator columns for trash case.
+ */
+#define OVERRIDE_ITERATOR_TRASH_COLUMNS                                     \
+ {                                                                          \
+   { "overrides_trash.id", "id", KEYWORD_TYPE_INTEGER },                    \
+   { "overrides_trash.uuid", "uuid", KEYWORD_TYPE_STRING },                 \
+   { "CAST ('' AS TEXT)", NULL, KEYWORD_TYPE_STRING },                      \
+   { "CAST ('' AS TEXT)", NULL, KEYWORD_TYPE_STRING },                      \
+   { "overrides_trash.creation_time",                                       \
+     NULL,                                                                  \
+     KEYWORD_TYPE_INTEGER },                                                \
+   { "overrides_trash.modification_time",                                   \
+     NULL,                                                                  \
+     KEYWORD_TYPE_INTEGER },                                                \
+   { "overrides_trash.creation_time",                                       \
+     "created",                                                             \
+     KEYWORD_TYPE_INTEGER },                                                \
+   { "overrides_trash.modification_time",                                   \
+     "modified",                                                            \
+     KEYWORD_TYPE_INTEGER },                                                \
+   {                                                                        \
+     "(SELECT name FROM users WHERE users.id = overrides_trash.owner)",     \
+     "_owner",                                                              \
+     KEYWORD_TYPE_STRING                                                    \
+   },                                                                       \
+   { "owner", NULL, KEYWORD_TYPE_STRING },                                  \
+   /* Columns specific to overrides_trash. */                               \
+   { "overrides_trash.nvt", "oid", KEYWORD_TYPE_STRING },                   \
+   { "overrides_trash.text", "text", KEYWORD_TYPE_STRING },                 \
+   { "overrides_trash.hosts", "hosts", KEYWORD_TYPE_STRING },               \
+   { "overrides_trash.port", "port", KEYWORD_TYPE_STRING },                 \
+   { "severity_to_level (overrides_trash.severity, 1)",                     \
+     "threat",                                                              \
+     KEYWORD_TYPE_STRING },                                                 \
+   { "severity_to_level (overrides_trash.new_severity, 0)",                 \
+     "new_threat",                                                          \
+     KEYWORD_TYPE_STRING },                                                 \
+   { "overrides_trash.task", NULL, KEYWORD_TYPE_INTEGER },                  \
+   { "overrides_trash.result", "result", KEYWORD_TYPE_INTEGER },            \
+   { "overrides_trash.end_time", NULL, KEYWORD_TYPE_INTEGER },              \
+   {                                                                        \
+     "CAST (((overrides_trash.end_time = 0)"                                \
+     "       OR (overrides_trash.end_time >= m_now ())) AS INTEGER)",       \
+     "active",                                                              \
+     KEYWORD_TYPE_INTEGER                                                   \
+   },                                                                       \
+   {                                                                        \
+     "(CASE"                                                                \
+     " WHEN overrides_trash.nvt LIKE 'CVE-%%'"                              \
+     " THEN overrides_trash.nvt"                                            \
+     " ELSE (SELECT name FROM nvts WHERE oid = overrides_trash.nvt)"        \
+     " END)",                                                               \
+     "nvt",                                                                 \
+     KEYWORD_TYPE_STRING                                                    \
+   },                                                                       \
+   { "overrides_trash.nvt", "nvt_id", KEYWORD_TYPE_STRING },                \
+   {                                                                        \
+     "(SELECT uuid FROM tasks WHERE id = overrides_trash.task)",            \
+     "task_id",                                                             \
+     KEYWORD_TYPE_STRING                                                    \
+   },                                                                       \
+   {                                                                        \
+     "(SELECT name FROM tasks WHERE id = overrides_trash.task)",            \
+     "task_name",                                                           \
+     KEYWORD_TYPE_STRING                                                    \
+   },                                                                       \
+   { "overrides_trash.severity", NULL, KEYWORD_TYPE_DOUBLE },               \
+   { "overrides_trash.new_severity", NULL, KEYWORD_TYPE_DOUBLE },           \
+   { NULL, NULL, KEYWORD_TYPE_UNKNOWN }                                     \
+ }
+
 #endif // not _GVMD_MANAGE_SQL_OVERRIDES_H
