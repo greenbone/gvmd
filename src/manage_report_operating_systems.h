88ab75bffb84217d78c62c8b61a941a188081581
ozgen <ozgn45@gmail.com>
2026-04-23 14:18:22 +0200

    add: add manage helpers for get_report_operating_systems
    Add the SQL and manager helper functions needed to support
    the get_report_operating_systems command.
    
    This includes the required helpers in manage_sql_report_operating_systems and
    manage_report_operating_systems sources for fetching and counting report OS data.

---
+++ src/manage_report_operating_systems.h
@@ -0,0 +1,45 @@
+/* Copyright (C) 2026 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+/**
+ * @file
+ * @brief GVM management layer: Report operating systems.
+ *
+ * Non-SQL report operating systems code for the GVM management layer.
+ */
+
+#ifndef _GVM_MANAGE_REPORT_OPERATING_SYSTEMS_H
+#define _GVM_MANAGE_REPORT_OPERATING_SYSTEMS_H
+
+#include "manage_resources.h"
+
+#include <glib.h>
+
+struct report_os {
+ gchar *best_os_name;
+ gchar *os_cpe;
+ int hosts_count;
+};
+typedef struct report_os *report_os_t;
+
+report_os_t
+report_os_new (void);
+
+void
+report_os_free (report_os_t);
+
+GPtrArray *
+report_os_list_new (void);
+
+void
+report_os_list_free (GPtrArray *);
+
+int
+get_report_operating_systems (report_t,
+                              GPtrArray **);
+int
+report_operating_systems_count (report_t);
+
+#endif //_GVM_MANAGE_REPORT_OPERATING_SYSTEMS_H
