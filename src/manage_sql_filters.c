a04fc49ea1af4c35dacf81516647e2a8327d62e6
Robin Dittmar <robin.dittmar@greenbone.net>
2026-07-07 09:07:06 +0200

    updated sql filters to check `target_type` for import task

--- src/manage_sql_filters.c
+++ src/manage_sql_filters.c
@@ -1289,11 +1289,7 @@ filter_clause (const char* type, const char* filter,
                  (order,
                   " ORDER BY"
                   "  (CASE WHEN (SELECT "
-                  "               ( "
-                  "                  target = 0 "
-                  "                  and COALESCE( agent_group, 0) = 0 "
-                  "                  and COALESCE( oci_image_target, 0) = 0"
-                  "               ) "
+                  "               ( target_type = 0 ) " //TASKS_TARGET_TYPE_IMPORT_TASK
                   "              FROM tasks"
                   "              WHERE tasks.id = task)"
                   "    THEN '0'"
@@ -1310,10 +1306,7 @@ filter_clause (const char* type, const char* filter,
                 g_string_append_printf
                  (order,
                   " ORDER BY"
-                  "  (CASE WHEN ( target = 0 "
-                  "               and COALESCE( agent_group, 0) = 0 "
-                  "               and COALESCE( oci_image_target, 0) = 0"
-                  "             ) "
+                  "  (CASE WHEN ( target_type = 0 ) " //TASKS_TARGET_TYPE_IMPORT_TASK
                   "    THEN '0'"
                   "    ELSE run_status_name (run_status)"
                   "         || (SELECT CAST (temp / 100 AS text)"
@@ -1494,11 +1487,7 @@ filter_clause (const char* type, const char* filter,
                  (order,
                   " ORDER BY"
                   "  (CASE WHEN (SELECT "
-                  "               ( "
-                  "                  target = 0 "
-                  "                  and COALESCE( agent_group, 0) = 0 "
-                  "                  and COALESCE( oci_image_target, 0) = 0"
-                  "               ) "
+                  "               ( target_type = 0 ) " //TASKS_TARGET_TYPE_IMPORT_TASK
                   "              FROM tasks"
                   "              WHERE tasks.id = task)"
                   "    THEN '0'"
@@ -1515,10 +1504,7 @@ filter_clause (const char* type, const char* filter,
                 g_string_append_printf
                  (order,
                   " ORDER BY"
-                  "  (CASE WHEN ( target = 0 "
-                  "               and COALESCE( agent_group, 0) = 0 "
-                  "               and COALESCE( oci_image_target, 0) = 0"
-                  "             ) "
+                  "  (CASE WHEN ( target_type = 0 ) " //TASKS_TARGET_TYPE_IMPORT_TASK
                   "    THEN '0'"
                   "    ELSE run_status_name (run_status)"
                   "         || (SELECT CAST (temp / 100 AS text)"
