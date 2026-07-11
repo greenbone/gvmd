2686699a8e82cb033991005b7d4703e5886e3ed5
Matt Mundell <matt@mundell.me>
2026-07-11 23:45:12 +0200

    Fix: free host times in create_report_data_reset

--- src/gmp.c
+++ src/gmp.c
@@ -981,6 +981,8 @@ create_report_data_reset (create_report_data_t *data)
   free (data->result_qod);
   free (data->result_scan_nvt_version);
   free (data->result_severity);
+  free (data->host_end_host);
+  free (data->host_start_host);
 
   memset (data, 0, sizeof (create_report_data_t));
 }
