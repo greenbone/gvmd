7838a29a7119c2d80cc00e474f0d09d6f309d396
Timo Pollmeier <timo.pollmeier@greenbone.net>
2026-09-14 15:43:26 +0200

    Add new manage_... files for vt_tech_info

---
+++ src/manage_vt_tech_info.h
@@ -0,0 +1,21 @@
+/* Copyright (C) 2026 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+/**
+ * @file
+ * @brief GVM management layer: VT Technical Info.
+ *
+ *Vulnerability test optional metadata headers for the GVM management layer.
+ */
+
+#ifndef _GVMD_MANAGE_VT_TECH_INFO_H
+#define _GVMD_MANAGE_VT_TECH_INFO_H
+
+#include <glib.h>
+
+gchar *
+vt_tech_info_description_md_by_vt_id (const char*);
+
+#endif /* _GVMD_MANAGE_SQL_VT_TECH_INFO_H */
