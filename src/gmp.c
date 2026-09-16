42be798ca36854ab68e37662110ebddee006b686
ozgen <ozgn45@gmail.com>
2026-09-16 13:34:17 +0200

    add: implement export_delta_audit_report GMP command
    Add GMP support for asynchronous delta audit report exports.
    
    Validate report and delta report inputs, create or reuse matching report
    exports, and return the export UUID and current status.

--- src/gmp.c
+++ src/gmp.c
@@ -88,6 +88,7 @@
 #include "gmp_audit_report.h"
 #include "gmp_audit_report_exports.h"
 #include "gmp_audit_report_hosts.h"
+#include "gmp_delta_audit_report_exports.h"
 #include "gmp_delta_scan_report_exports.h"
 #include "gmp_scan_report_exports.h"
 #include "gmp_integration_configs.h"
@@ -4649,6 +4650,7 @@ typedef enum
   CLIENT_DOWNLOAD_REPORT_EXPORT,
   CLIENT_EMPTY_TRASHCAN,
   CLIENT_EXPORT_AUDIT_REPORT,
+  CLIENT_EXPORT_DELTA_AUDIT_REPORT,
   CLIENT_EXPORT_DELTA_SCAN_REPORT,
   CLIENT_EXPORT_SCAN_REPORT,
 #if ENABLE_AGENTS
@@ -5554,6 +5556,13 @@ gmp_xml_handle_start_element (/* unused */ GMarkupParseContext* context,
                                        attribute_names,
                                        attribute_values);
             set_client_state (CLIENT_EXPORT_AUDIT_REPORT);
+          }
+        else if (strcasecmp ("EXPORT_DELTA_AUDIT_REPORT", element_name) == 0)
+          {
+            export_delta_audit_report_start (gmp_parser,
+                                            attribute_names,
+                                            attribute_values);
+            set_client_state (CLIENT_EXPORT_DELTA_AUDIT_REPORT);
           }
         else if (strcasecmp ("EXPORT_DELTA_SCAN_REPORT", element_name) == 0)
           {
@@ -22597,6 +22606,10 @@ gmp_xml_handle_end_element (/* unused */ GMarkupParseContext* context,
 
     case CLIENT_EXPORT_AUDIT_REPORT:
       export_audit_report_element_end (gmp_parser, error, element_name);
+      break;
+
+    case CLIENT_EXPORT_DELTA_AUDIT_REPORT:
+      export_delta_audit_report_element_end (gmp_parser, error, element_name);
       break;
 
     case CLIENT_EXPORT_DELTA_SCAN_REPORT:
